cc76a2f6f3f7950fb89c22397cc4283225f28eff
Ben Roberts <benroberts999@gmail.com>
2018-01-21 12:45:07 -0800

    misc

--- test.cpp
+++ test.cpp
@@ -1,10 +1,13 @@
-// #include "ElectronOrbitals.h"
-// #include "physicalConstants.h"
-// #include "atomInfo.h"
-// #include "adamsSolveLocalBS.h"
+#include "ElectronOrbitals.h"
+#include "physicalConstants.h"
+#include "atomInfo.h"
+#include "adamsSolveLocalBS.h"
 #include <iostream>
 
 
 int main(void){
   std::cout<<"Hello world!\n";
+
+  ElectronOrbitals wf(1,1,1000);
+
 }
