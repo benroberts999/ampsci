674aaf582b2e9182a76645ef5417060f6b65cfbd
B M Roberts <benroberts999@gmail.com>
2018-04-04 18:35:02 -0700

    Essentially working - need to modify class

--- src/adamsSolveLocalContinuum.cpp
+++ src/adamsSolveLocalContinuum.cpp
@@ -17,21 +17,148 @@ Also, need at least ~10 points per half-period. More points for higher energy!
 
 
 //******************************************************************************
-int solveContinuum(std::vector<double> &p, std::vector<double> &q, double en,
+int solveContinuum(std::vector<double> &p, std::vector<double> &q, double ec,
     std::vector<double> v, int Z, int ka,
-    std::vector<double> r, std::vector<double> drdt, double h, int NGP,
+    std::vector<double> rc, std::vector<double> drdt, double h,
+    int NGPb, int NGPc, int i_asym,
     double alpha)
 {
 
   //Perform the "outwards integration"
-  outwardAM(p,q,en,v,Z,ka,r,drdt,h,NGP,NGP-1,alpha);
-  // XXX shuod be Z or Zion here?? XXX
+  std::vector<double> pc(NGPc),qc(NGPc);
+  outwardAM(pc,qc,ec,v,Z,ka,rc,drdt,h,NGPc,NGPc-1,alpha);
+
+  //Find a better (lower) asymptotic region:
+  i_asym = findAsymptoticRegion(pc,rc,NGPb,NGPc,i_asym);
+
+  // Find amplitude of large-r (asymptotic region) sine-like wf
+  double amp = findSineAmplitude(pc,rc,NGPc,i_asym);
+
+  // Calculate normalisation coeficient, D, and re-scaling factor:
+  // D = Sqrt[alpha/(pi*eps)] <-- Amplitude of large-r p(r)
+  // eps = Sqrt[en/(en+2mc^2)]
+  double al2 = pow(alpha,2);
+  double ceps = sqrt(ec/(ec*al2+2.)); // c*eps = eps/alpha
+  double D = 1./sqrt(M_PI*ceps);
+  double sf=D/amp; //re-scale factor
+
+  //Normalise the wfs, and transfer back to shorter arrays:
+  for(int i=0; i<NGPb; i++){
+    p[i] = sf*pc[i];
+    q[i] = sf*qc[i];
+  }
+
+  //XXX TEST: Output wfs:
+  std::ofstream ofile,ofileN;
+  ofileN.open("contN.txt");
+  ofile.open("cont.txt");
+  for(int i=0; i<NGPc; i++){
+    ofile<<rc[i]<<" "<<pc[i]<<" "<<qc[i]<<"\n";
+    ofileN<<rc[i]<<" "<<sf*pc[i]<<" "<<sf*qc[i]<<"\n";
+  }
+  ofileN.close();
+  ofile.close();
 
 
   return 0;
 }
 
 
+//******************************************************************************
+double findSineAmplitude(std::vector<double> pc, std::vector<double> rc,
+  int NGPc, int i_asym)
+/*
+ Find "maximum" amplitude, by using a quadratic fit to 2 nearest points
+ Scale by ratio of this maximum to max of analytic soln
+*/
+{
+
+  int ntry=0, maxtry=5;
+  double amp=0;
+  while(ntry<maxtry){
+    //find first zero after r_asym
+    for(int i=i_asym; i<NGPc; i++){
+      if(pc[i]*pc[i-1]<0){
+        i_asym=i;
+        break;
+      }
+    }
+    //find max:
+    double y0=0,y1=0,y2=0,y3=0,y4=0;
+    double x0=0,x1=0,x2=0,x3=0,x4=0;
+    for(int i=i_asym+1; i<NGPc; i++){
+      if(fabs(pc[i])<fabs(pc[i-1])){
+        y0=fabs(pc[i-3]);
+        y1=fabs(pc[i-2]);
+        y2=fabs(pc[i-1]);
+        y3=fabs(pc[i]);
+        y4=fabs(pc[i+1]);
+        x0=rc[i-3];
+        x1=rc[i-2];
+        x2=rc[i-1];
+        x3=rc[i];
+        x4=rc[i+1];
+        break;
+      }
+    }
+    i_asym++;
+    ntry++;
+    double out1 = fitQuadratic(x1,x2,x3,y1,y2,y3);
+    double out2 = fitQuadratic(x0,x2,x4,y0,y2,y4);
+    amp+=0.5*(out1+out2);
+  }
+
+  return amp/=maxtry;
+}
+
+//******************************************************************************
+int findAsymptoticRegion(std::vector<double> pc, std::vector<double> rc,
+  int NGPb, int NGPc, int i_asym)
+/*
+Finds a 'better' guess for the asymptotic region, by looking for where
+the period of oscilations becomes constant
+(variation in periof drops below certain value)
+
+XXX Note: this method only 'kinda' works???
+ a) am I not going out far enough?
+ b) Or, it's just not that accurate? Seems acurate to 1 - 0.1% ?
+
+*/
+{
+  // Find the r's for psi=zero, two consec => period
+  //Once period is converged enough, can normalise by comparison with
+  // exact (asymptotic) solution (??)
+  //nb: looks for convergence between r(NGP) and r_asym
+  // If doesn't 'converge' in this region, uses r_asym
+  double xa=1,xb=pc[NGPb]; //NGPb is #pts in regular grid
+  double wk1=-1, wk2=0;
+  for(int i=NGPb; i<i_asym; i++){
+    xa=xb;
+    xb=pc[i];
+    if(xb*xa<0){
+      //Use linear extrapolation to find exact r of the zero:
+      double r1 = (rc[i]*pc[i-1]-rc[i-1]*pc[i])/(pc[i-1]-pc[i]);
+      double ya=xb,yb=xb;
+      for(int j=i+1; j<NGPc; j++){
+        ya=yb;
+        yb=pc[j];
+        if(ya*yb<0){
+          //Use linear extrapolation to find exact r of the next zero:
+          double r2 = (rc[j]*pc[j-1]-rc[j-1]*pc[j])/(pc[j-1]-pc[j]);
+          wk1 = wk2;
+          wk2 = r2-r1;
+          break;
+        }
+      }
+      if(fabs(wk1-wk2)<1.e-4){
+        //check for "convergence"
+        i_asym=i;
+        break;
+      }
+    }
+  }
+  return i_asym;
+}
 
 //******************************************************************************
 double fitQuadratic(double x1, double x2, double x3,
--- src/h-like.cpp
+++ src/h-like.cpp
@@ -29,24 +29,6 @@ int main(void){
   printf("\nRunning SolveDBS for Local H-like potential, Z=%i\n",Z);
   printf("*************************************************\n");
 
-//ngp=500000;
-
-
-
-//   //double out = fitQuadratic(1007.,1008.,1009.,1.39351,1.40745,1.2818,true);
-//   //double out = fitQuadratic(1007.5,1007.6,1007.7,1.41817,1.41886,1.41813,true);
-//   //double out = fitQuadratic(1007.0,1007.5,1008.,1.39351,1.41817,1.40745,true);
-//   double out = fitQuadratic(.0,.5,.1,6.,7.,5.);
-//   std::cout<<out<<"\n";
-//   printf("%.8f\n",out);
-//
-//
-// return 1;
-
-
-
-
-
 
   //Generate the orbitals object:
   ElectronOrbitals wf(Z,A,ngp,r0,rmax,varalpha);
@@ -128,10 +110,10 @@ int main(void){
   //std::cout<<"\n\n";
 
 
-  double ec = 0.25;
+  double ec = 0.2;
   int kc=-1;
-
   double Zion=1.;
+
   std::vector<double> rc=wf.r;
   std::vector<double> drdtc=wf.drdt;
   int NGPc=wf.ngp;
@@ -141,7 +123,6 @@ int main(void){
   double lam = 1.e7; //XXX ???
   double r_asym = (Zion + sqrt(4.*lam*ec+pow(Zion,2)))/(2.*ec);
 
-  //XXX testing only!!!
   double hc = wf.h;
   double hc_target = (M_PI/15)/sqrt(2.*ec);
   if(hc>hc_target){
@@ -156,8 +137,10 @@ int main(void){
 
   //Set up temporary continuum grid:
   // Extend grid for continuum state. Goes to ~20% more than 'anymp region'
+  int i_asym = wf.ngp-1;
   while(true){
     double r_new = last_r + hc;
+    if(r_new>=r_asym && last_r<r_asym) i_asym=NGPc-1;
     rc.push_back(r_new);
     drdtc.push_back(hc/wf.h); //??? correct??
     vc.push_back(-Zion/r_new);
@@ -167,135 +150,8 @@ int main(void){
   }
 
   //Solve Dirac equation on temporary (extended) grid
-  std::vector<double> pc(NGPc),qc(NGPc);
-  solveContinuum(pc,qc,ec,vc,wf.Z,kc,rc,drdtc,wf.h,NGPc,wf.alpha);
-
-
-  std::ofstream ofile;
-  ofile.open("cont.txt");
-  for(int i=0; i<NGPc; i++){
-    ofile<<rc[i]<<" "<<pc[i]<<" "<<qc[i]<<"\n";
-  }
-  ofile.close();
-
-  //find index of 'asymptotically large r' region
-  int i0 = wf.ngp;
-  for(int i=wf.ngp; i<NGPc; i++){
-    if(rc[i]>r_asym){
-      i0=i-1;
-      break;
-    }
-  }
-  //std::cout<<"\nr_asym="<<r_asym<<", i0="<<i0<<"\n\n";
-
-
-  // Find the r's for psi=zero, two consec => period
-  //Once period is converged enough, can normalise by comparison with
-  // exact (asymptotic) solution (??)
-  //nb: looks for convergence between r(NGP) and r_asym
-  // If doesn't 'converge' in this region, uses r_asym
-  double xa=1,xb=pc[wf.ngp];
-  double wk1=-1, wk2=0;
-  for(int i=wf.ngp; i<i0; i++){
-    xa=xb;
-    xb=pc[i];
-    if(xb*xa<0){
-      //Use linear extrapolation to find exact r of the zero:
-      double r1 = (rc[i]*pc[i-1]-rc[i-1]*pc[i])/(pc[i-1]-pc[i]);
-      double ya=xb,yb=xb;
-      for(int j=i+1; j<NGPc; j++){
-        ya=yb;
-        yb=pc[j];
-        if(ya*yb<0){
-          //Use linear extrapolation to find exact r of the next zero:
-          double r2 = (rc[j]*pc[j-1]-rc[j-1]*pc[j])/(pc[j-1]-pc[j]);
-          wk1 = wk2;
-          wk2 = r2-r1;
-          break;
-        }
-      }
-      if(fabs(wk1-wk2)<1.e-4){
-        //check for "convergence"
-        i0=i;
-        break;
-      }
-    }
-  }
-  //XXX Note: this method only 'kinda' works???
-  // a) am I not going out far enough?
-  // b) Or, it's just not that accurate? Seems acurate to 1 - 0.1% ?
-
-
-  // Find "maximum" amplitude, by using a quadratic fit to 2 nearest points
-  // Scale by ratio of this maximum to max of analytic soln
-  int ntry=0, maxtry=5;
-  double amp=0;
-  while(ntry<maxtry){
-    //find first zero after r_asym
-    for(int i=i0; i<NGPc; i++){
-      if(pc[i]*pc[i-1]<0){
-        i0=i;
-        break;
-      }
-    }
-    //find max:
-    int imax=0;
-    double y0,y1,y2,y3,y4;
-    double x0,x1,x2,x3,x4;
-    for(int i=i0+1; i<NGPc; i++){
-      if(fabs(pc[i])<fabs(pc[i-1])){
-        imax=i-1;
-        y0=fabs(pc[i-3]);
-        y1=fabs(pc[i-2]);
-        y2=fabs(pc[i-1]);
-        y3=fabs(pc[i]);
-        y4=fabs(pc[i+1]);
-        x0=rc[i-3];
-        x1=rc[i-2];
-        x2=rc[i-1];
-        x3=rc[i];
-        x4=rc[i+1];
-        break;
-      }
-    }
-    i0++;
-    ntry++;
-    double out1 = fitQuadratic(x1,x2,x3,y1,y2,y3);
-    double out2 = fitQuadratic(x0,x2,x4,y0,y2,y4);
-    amp+=0.5*(out1+out2);
-  }
-  amp/=maxtry;
-
-  // Calculate normalisation coeficient, D, and re-scaling factor:
-  // D = Sqrt[alpha/(pi*eps)] <-- Amplitude of large-r p(r)
-  // eps = Sqrt[en/(en+2mc^2)]
-  double al2 = pow(wf.alpha,2);
-  double ceps = sqrt(ec/(ec*al2+2.)); // c*eps = eps/alpha
-  //printf("%.10f\n",ceps);
-  double D = 1./sqrt(M_PI*ceps);
-  //printf("%.10f\n",D);
-  double sf=D/amp; //re-scale factor
-  //Normalise the wfs:
-  for(int i=0; i<NGPc; i++){
-    // XXX here: transfere into 'true' p,q (from temp ones!)
-    pc[i] *= sf;
-    qc[i] *= sf;
-  }
-
-
-
-
-
-  //std::ofstream ofile;
-  ofile.open("contN.txt");
-  for(int i=0; i<NGPc; i++){
-    ofile<<rc[i]<<" "<<pc[i]<<" "<<qc[i]<<"\n";
-  }
-  ofile.close();
-
-
-
-
+  std::vector<double> pc(wf.ngp),qc(wf.ngp);
+  solveContinuum(pc,qc,ec,vc,wf.Z,kc,rc,drdtc,hc,wf.ngp,NGPc,i_asym,wf.alpha);
 
 
   return 0;
