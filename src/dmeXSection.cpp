fb50b5cc073562557db5b168cc08fff3e80bc516
B M Roberts <benroberts999@gmail.com>
2018-08-07 20:12:51 +0200

    adding q integrals

--- src/dmeXSection.cpp
+++ src/dmeXSection.cpp
@@ -48,47 +48,68 @@ int main(void){
   std::cout<<qsteps<<" "<<num_states<<" "<<desteps<<"\n";
 
 
-  double m=1; //XXX in GeV
+  double m=1.; //XXX in GeV
   //convert WIMP masses from GeV to a.u.
   m *= (1.e3/FPC::m_e_MeV);
 
   bool finite_med = true;
   //Vector mass:
   //NB: have both '0' case (easy), and 'inf' case! (changes coupling const!)
-  double mv = 0.1;
+  double mv = 10.;
   //convert from MeV to au:
   mv *= (1./FPC::m_e_MeV);
 
 
-  double v=137*1.e-3; // typical v..integrate later...
+  double v=137*1.e-2; // typical v..integrate later...
 
-  double dE = demin;
+  //double dE = demin;
 
-  double arg = pow(m*v,2)-2.*m*dE; //may be negative; skip!
-  double qminus = m*v - sqrt(arg);
-  double qplus  = m*v + sqrt(arg);
+
 
   //Numberical constant. Note: Inlcudes dqonq
   //NOTE: if alpgha_chi = alpha in the code.. kill c2 !!
   double Aconst = 8*M_PI*FPC::c2*dqonq;
-  std::cout<<"\n\n Aconst="<<Aconst<<"\n\n";
-
-  std::cout<<"\n\n demax="<<demax*FPC::Hartree_eV<<"\n\n";
+  std::cout<<"\nAconst="<<Aconst<<"\n";
+  std::cout<<"\n\n demin="<<demin*FPC::Hartree_eV<<"\n";
+  std::cout<<"demax="<<demax*FPC::Hartree_eV<<"\n\n";
 
   //find clostest dE to given "target"!
   //How to do q-grid integration?
 
+  //XXX XXX XXX the 3s state showing zero... WHY?!?!?!
+
+  //XXX define a v grid. Sort f(v) into Array.
+  // This a) speeds it up
+  // b) easier function-swapping
+  // c) convert to atomic units
+
+for(int ie=0; ie<desteps; ie++){
   double a=0;
-  for(int iq=0; iq<qsteps; iq++){
-    double x = double(iq)/(qsteps-1);
-    double q = qmin*pow(qmax/qmin,x);
-    double dq_on_dqonq = q; //devide by dqonq - just a const. Include in Acont!
-    double Fq = q*dq_on_dqonq;
-    if(finite_med) Fq /= pow(q*q+mv*mv,2);
-    if(q<qminus || q>qplus) continue;
-    a+=(1./v)*Fq*AKenq[1][2][iq];// xxx choose state + dE!
+  double xe = double(ie)/(desteps-1); //XXX allow for single dE !!
+  double dE = demin*pow(demax/demin,xe);
+  double arg = pow(m*v,2)-2.*m*dE; //may be negative; skip!
+  if(arg<0) continue;
+  double qminus = m*v - sqrt(arg);
+  double qplus  = m*v + sqrt(arg);
+  std::cout<<ie<<" "<<qminus<<" "<<qplus<<"\n";
+  for(int ink=0; ink<num_states; ink++){
+    double a_nk=0;
+    for(int iq=0; iq<qsteps; iq++){
+      double x = double(iq)/(qsteps-1);
+      double q = qmin*pow(qmax/qmin,x);
+      if(q<qminus || q>qplus) continue;
+      double dq_on_dqonq = q; //devide by dqonq - just a const. Include in Acont!
+      double Fq = q*dq_on_dqonq;
+      if(finite_med) Fq /= pow(q*q+mv*mv,2);
+      a_nk+=(1./v)*fv(v)*Fq*AKenq[ie][ink][iq];// xxx choose state + dE!
+    }
+    std::cout<<nklst[ink]<<"; a="<<a_nk*Aconst<<"\n";
+    a+=a_nk;
   }
-  std::cout<<"a="<<a*Aconst<<"\n";
+  std::cout<<" "<<ie<<"; a="<<a*Aconst<<"\n";
+}
+
+std::cout<<AKenq[0][2][100]<<"\n";
 
 
   //Min/max m_chi
