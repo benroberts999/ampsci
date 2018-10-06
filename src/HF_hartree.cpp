e33955a05cc5c61d0777186f72651f6c36cc0b22
Ben Roberts <benroberts999@gmail.com>
2018-10-06 23:55:47 +0200

    Calculates v_ex - NOT TESTED

--- src/HF_hartree.cpp
+++ src/HF_hartree.cpp
@@ -137,56 +137,81 @@ core=true by default
   return 0;
 }
 
-
+//==============================================================================
 
 //******************************************************************************
-int formVex(ElectronOrbitals &wf, int a, std::vector<double> vexa)
-/*
-for now, include a in the b summation too.
-Means we should not scale v_dir?
-v_tot(r) should still go to (N-M)/r for large r - check!
-Should I calc+store v_ex for each core state ? Yes, probs. Can //-ize
-OR - run HF its for each core state sepp.?
-*/
-{
-  int ngp = wf.ngp;
-  std::vector<double> vex_a(ngp);
-  for(int i=0; i<ngp; i++){
-    double vex_a_r = 0;
-    double fac_bot = wf.p[a][i]*wf.p[a][i] + wf.q[a][i]*wf.q[a][i];
-    for(int b=0; b<wf.num_core_states; b++){
-      int tjb = ATI::twoj_k(wf.kappa[b]); //XXX swap r and b loops?
-      double fac_top = wf.p[a][i]*wf.p[b][i] + wf.q[a][i]*wf.q[b][i];
-      //Calculate vex_ab_r
-      vex_a_r += (tjb+1)*vex_ab_r*fac_top/fac_bot; //XXX * fill factor!!
-    }
+int formVexCore(ElectronOrbitals &wf, std::vector< std::vector<double> > &vex){
+
+  vex.clear();
+  vex.resize(num_core_states);
+  #pragma omp parallel for
+  for(int a=0; a<wf.num_core_states; a++){
+    std::vector<double> vex_a;
+    formVexA(wf,a,vex_a);
+    vex[a] = vex_a; //double-check this works w/ vectors..
   }
 
 }
 
 //******************************************************************************
-double vexABr(ElectronOrbitals &wf, int a, int b, int ir)
+int formVexA(ElectronOrbitals &wf, int a, std::vector<double> &vex_a)
+/*
+for now, include a in the b summation too.
+Means we should not scale v_dir?
+v_tot(r) should still go to (N-M)/r for large r - check!
+*/
+{
+  int ngp = wf.ngp;
+
+  int tja = ATI::twoj_k(wf.kappa[a]);
+  int la = ATI::l_a(wf.kappa[a]);
+
+  vex_a.clear();
+  vex_a.resize(ngp);
+  for(int b=0; b<wf.num_core_states; b++){// Core? or all??
+    int tjb = ATI::twoj_k(wf.kappa[b]);
+    int lb = ATI::l_a(wf.kappa[b]);
+    std::vector<double> L_abk;
+    int k_min = formLambdaABk(L_abk,tja,tjb,la,lb);
+    double stf = wf.core_ocf[b]*(tjb+1); //avg over non-rel configs
+    for(int ir=0; ir<ngp; ir++){
+      double fac_bot = wf.p[a][ir]*wf.p[a][ir] + wf.q[a][ir]*wf.q[a][ir]; //?
+      double fac_top = wf.p[a][ir]*wf.p[b][ir] + wf.q[a][ir]*wf.q[b][ir];
+      double vex_ab_r = vexABr(wf,a,b,ir,L_abk,k_min);
+      vex_a[ir] += stf*vex_ab_r*fac_top/fac_bot;
+    }
+  }
+  return 0;
+}
+
+//******************************************************************************
+formLambdaABk(std::vector<double> &L_abk, int tja, int tjb, int la, int lb)
+/*
+L_abk = 3js(ja,jb,k,-1/2,1/2,2)^2 * parity(la+lb+k)
+*/
+{
+  int k_min = fabs(tja - tjb)/2;
+  int k_max = (tja + tjb)/2;
+  L_abk.resize(k_max);
+  for(int k = k_min; k<=k_max; k++){
+    double tL = pow(WIG::threej_2(tja,tjb,2*k,-1,1,0),2);
+    tL *= WIG::parity(la,lb,k);
+    L_abk[k] = tL;
+  }
+  return k_min;
+}
+
+//******************************************************************************
+double vexABr(ElectronOrbitals &wf, int a, int b, int ir,
+  std::vector<double> &L_abk, int k_min)
 /*
 Not sure if this is the best way to do this..
 vex_ab(r) = sum_k L_abk * vex_abk(r)   [2jb+1 already above]
-L_abk = 3js(ja,jb,k,-1/2,1/2,2)^2 * parity(ja+jb+k)
 v_abk(r) = int [r_min^k/r_max^(k+1)]*[fafb + gagb](r2) dr_2
 r_min = min(r,r2) //nb: can test the indices!
 */
 {
-
-  int tja = ATI::twoj_k(wf.kappa[a]);
-  int tjb = ATI::twoj_k(wf.kappa[a]);
-
-  int k_min = fabs(tja - tjb)/2;
-  int k_max = (tja + tjb)/2;
-
-  std::vector<double> L_abk(k_max);
-  for(int k = k_min; k<=k_max; k++){
-    double tL = pow(WIG::threej_2(tja,tjb,2*k,-1,1,0),2);
-    tL *= parity(la,lb,k);
-    L_abk[k] = tL;
-  }
+  int k_max = (int) L_abk.size();
 
   double vex_ab_r = 0;
   for(int jr=0; jr<wf.ngp; jr++){
@@ -201,73 +226,73 @@ r_min = min(r,r2) //nb: can test the indices!
     double FF = (wf.p[a][jr]*wf.p[b][jr] + wf.q[a][jr]*wf.q[b][jr]);
     vex_ab_r += xL*FF*wf.drdt[jr];
   }
-  return vex_ab_r*wf.h; // XXX Better to *h outside? Maybe, but confusing!
+  return vex_ab_r*wf.h;
 }
 
-//******************************************************************************
-int vkScreeningOLD(ElectronOrbitals &wf, int a, int b, std::vector<double> vk)
-/*
-Pass by reference? Faster? Worse??
-Averaged exchange potential! Break into functions??
-NOTE: must be multiplied
-*/
-
-{
-
-  vk.resize(wf.ngp);
-
-  int ka = wf.kappa[a];
-  int kb = wf.kappa[b];
-  int tja = ATI::twoj_k(ka);
-  int tjb = ATI::twoj_k(kb);
-  int la = ATI::l_k(ka);
-  int lb = ATI::l_k(kb);
-
-  int ja_m_jb = fabs(tja - tjb)/2;
-  int ja_p_jb = (tja + tjb)/2;
-
-  //Stats factors (averaged over non-rel configs)
-  double sfa = (tja+1)*wf.core_ocf[a];
-  double sfb = (tjb+1)*wf.core_ocf[b];
-
-  //Count number of electrons in the core
-  int Ncore=0;
-  for(size_t i=0; i<wf.core_list.size(); i++) Ncore+=wf.core_list[i];
-
-  std::vector<double> Lam_abk;
-  std::vector<int> k_list;
-  for(int k = ja_m_jb; k<=ja_p_jb; k++){
-    double tL = pow(WIG::threej_2(tja,tjb,2*k,-1,1,0),2);
-    tL *= parity(la,lb,k);
-    if(tL != 0){
-      k_list.push_back(k);
-      Lam_abk.push_back(tL);
-    }
-  }
-  if(Lam_abk.size()==0) return 0;
-
-  #pragma omp parallel for
-  for(int i=0; i<wf.ngp; i++){
-    double r = wf.r[i];
-
-    double v_tmp = 0;
-    for(int j=0; j<wf.ngp; j++){
-      double rp = wf.r[j];
-      double t_X = (wf.p[a][j]*wf.p[b][j] + wf.q[a][j]*wf.q[b][j])*wf.drdt[i];
-      for(size_t ik=0; ik<k_list.size(); ik++){
-        int k = k_list[ik];
-        double tL = Lam_abk[ik];
-        double x_k;
-        if(rp<r) x_k = pow(rp/r,k)/r;
-        else     x_k = pow(r/rp,k)/rp;
-        v_tmp += tL*x_k*t_X;
-      }
-    }
-    double sf = sfa*sfb*(1./Ncore)
-    v_tmp *= sf*wf.h*(wf.p[a][i]*wf.p[b][i] + wf.q[a][i]*wf.q[b][i]);
-
-  }
-}
+// //******************************************************************************
+// int vkScreeningOLD(ElectronOrbitals &wf, int a, int b, std::vector<double> vk)
+// /*
+// Pass by reference? Faster? Worse??
+// Averaged exchange potential! Break into functions??
+// NOTE: must be multiplied
+// */
+//
+// {
+//
+//   vk.resize(wf.ngp);
+//
+//   int ka = wf.kappa[a];
+//   int kb = wf.kappa[b];
+//   int tja = ATI::twoj_k(ka);
+//   int tjb = ATI::twoj_k(kb);
+//   int la = ATI::l_k(ka);
+//   int lb = ATI::l_k(kb);
+//
+//   int ja_m_jb = fabs(tja - tjb)/2;
+//   int ja_p_jb = (tja + tjb)/2;
+//
+//   //Stats factors (averaged over non-rel configs)
+//   double sfa = (tja+1)*wf.core_ocf[a];
+//   double sfb = (tjb+1)*wf.core_ocf[b];
+//
+//   //Count number of electrons in the core
+//   int Ncore=0;
+//   for(size_t i=0; i<wf.core_list.size(); i++) Ncore+=wf.core_list[i];
+//
+//   std::vector<double> Lam_abk;
+//   std::vector<int> k_list;
+//   for(int k = ja_m_jb; k<=ja_p_jb; k++){
+//     double tL = pow(WIG::threej_2(tja,tjb,2*k,-1,1,0),2);
+//     tL *= parity(la,lb,k);
+//     if(tL != 0){
+//       k_list.push_back(k);
+//       Lam_abk.push_back(tL);
+//     }
+//   }
+//   if(Lam_abk.size()==0) return 0;
+//
+//   #pragma omp parallel for
+//   for(int i=0; i<wf.ngp; i++){
+//     double r = wf.r[i];
+//
+//     double v_tmp = 0;
+//     for(int j=0; j<wf.ngp; j++){
+//       double rp = wf.r[j];
+//       double t_X = (wf.p[a][j]*wf.p[b][j] + wf.q[a][j]*wf.q[b][j])*wf.drdt[i];
+//       for(size_t ik=0; ik<k_list.size(); ik++){
+//         int k = k_list[ik];
+//         double tL = Lam_abk[ik];
+//         double x_k;
+//         if(rp<r) x_k = pow(rp/r,k)/r;
+//         else     x_k = pow(r/rp,k)/rp;
+//         v_tmp += tL*x_k*t_X;
+//       }
+//     }
+//     double sf = sfa*sfb*(1./Ncore)
+//     v_tmp *= sf*wf.h*(wf.p[a][i]*wf.p[b][i] + wf.q[a][i]*wf.q[b][i]);
+//
+//   }
+// }
 
 
 }//Namespace
