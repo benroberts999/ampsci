bba5752bfa4fe9f73ff10e270e374b04b0688f83
B M Roberts <benroberts999@gmail.com>
2018-11-13 18:03:06 +0100

    Update SHM values and normalisation. Allows error terms

--- src/SHM_standardHaloModel.h
+++ src/SHM_standardHaloModel.h
@@ -3,13 +3,25 @@
 
 namespace SHM{
 
-  const double vesc = 544.; // galactic escape velocity
-  const double vL0 = 220.; // local frame velocity, average
-  const double vc  = 220; // circular velocity
-  const double vearth = 30.; //XXX update! ??
+  const double VESC = 550.; // galactic escape velocity [1804.01231]
+  const double DEL_VESC = 55.;
+  //Range: 498 - 608
 
-  const double max_v = vesc + vL0 + vearth;
+  const double V0  = 220;   //1804.01231 + RevModPhys.85.1561 OR 235
+  const double DEL_V0 = 20.; //range 220 - 235
 
-  double fv(double v, double phi);
+  const double VSUN = V0 + 13; // [1804.01231] - or 220 {older}
+  //const double DEL_VSUN = 15.; //same as above!
+
+  const double VEORB = 29.8; //[RevModPhys.85.1561]
+
+  const double COSBETA = 0.49; // 1804.01231 (Earth inclination to sun dir)
+
+  const double VEROTEQ = 0.47; //earth rotation speed (approx) @ equator
+
+  const double MAXV = VESC + VSUN + VEORB + VEROTEQ;
+
+  double fv(double v, double sinphi=0, double dves=0, double dv0=0);
+  double normfv(double sinphi=0, double dves=0, double dv0=0);
 
 }
