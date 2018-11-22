56552c2ba39eb2de33f66f2c8845212b77bd9c9e
B M Roberts <benroberts999@gmail.com>
2018-11-22 18:52:33 +0100

    Adds standard halo model class that does normalisation

---
+++ src/StandardHaloModel.cpp
@@ -0,0 +1,72 @@
+#include "StandardHaloModel.h"
+
+using namespace SHMCONSTS;
+
+
+StandardHaloModel::StandardHaloModel(double in_cosphi, double dves, double dv0)
+{
+  cosphi = in_cosphi;
+
+  v0    = V0   + dv0*DEL_V0;   //account for errors
+  v_sun = VSUN + dv0*DEL_V0;   //account for errors
+  vesc  = VESC + dves*DEL_VESC; //account for errors
+  veorb = VEORB;
+
+  NormConst = 1;
+  double tmp = normfv();
+  NormConst = tmp;
+}
+
+
+//******************************************************************************
+double StandardHaloModel::fv(double v)
+/*
+Standard halo model for velocity distribution, in laboratory frame.
+ f ~ v^2 exp(-v^2)
+Note: distribution for DM particles that cross paths with Earth.
+
+Note: normalised for all extras = 0.
+Otherwise, NOT normalised!
+
+ sinphi should be [-1,1]
+ dv0 should be [0,1]..
+ dves = [-1,1]
+*/
+{
+  //local velocity (lab)
+  double vl = v_sun + veorb*COSBETA*cosphi;
+
+  //Norm const * v^2:
+  double A = NormConst*pow(v,2);
+
+  double arg1 = -pow((v-vl)/v0,2);
+
+  if(v<=0){
+    return 0;
+  }else if(v<vesc-vl){ //here
+    double arg2 = -pow((v+vl)/v0,2);
+    return A*(exp(arg1)-exp(arg2));
+  }else if(v<vesc+vl){ //here
+    double arg2 = -pow(vesc/v0,2);
+    return A*(exp(arg1)-exp(arg2));
+  }else{
+    return 0;
+  }
+
+}
+
+
+//******************************************************************************
+double StandardHaloModel::normfv()
+{
+  int num_vsteps = 2000;
+  double dv = MAXV/num_vsteps;
+
+  double v = dv;
+  double A = 0;
+  for(int i=0; i<num_vsteps; i++){
+    A += fv(v);
+    v += dv;
+  }
+  return 1./(A*dv);
+}
---
+++ src/StandardHaloModel.h
@@ -0,0 +1,38 @@
+#ifndef _SHM_H
+#define _SHM_H
+#include <vector>
+#include <cmath>
+
+namespace SHMCONSTS{
+  const double VESC = 550.; // galactic escape velocity [1804.01231]
+  const double DEL_VESC = 55.;
+  //Range: 498 - 608
+
+  const double V0  = 220;   //1804.01231 + RevModPhys.85.1561 OR 235
+  const double DEL_V0 = 20.; //range 220 - 235
+
+  const double VSUN = V0 + 13; // [1804.01231]
+  //const double DEL_VSUN = 15.; //same as above!
+
+  const double VEORB = 29.8; //[RevModPhys.85.1561]
+
+  const double COSBETA = 0.49; // 1804.01231 (Earth inclination to sun dir)
+
+  const double VEROTEQ = 0.47; //earth rotation speed (approx) @ equator
+
+  const double MAXV = VESC + VSUN + VEORB + VEROTEQ + DEL_V0 + DEL_VESC;
+}
+
+//******************************************************************************
+class StandardHaloModel{
+public:
+  StandardHaloModel(double in_cosphi=0, double in_dves=0, double in_dv0=0);
+  double cosphi;
+  double v0,v_sun,vesc,veorb;
+  double fv(double v);
+private:
+  double NormConst = 1;
+  double normfv();
+};
+
+#endif
