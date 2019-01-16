f80291bdb6925356e04606b73ed7078804a6f44f
B M Roberts <benroberts999@gmail.com>
2019-01-16 19:22:16 +0100

    new quick wigner

--- src/wigner.cpp
+++ src/wigner.cpp
@@ -1,75 +1,87 @@
 #include "WIG_369j.h"
 #include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
+
 /*
 Quick routine that outputs numeric values for Wigner 3,6,9J symbols.
 */
 
-int caseCG(){
-  double j1, m1, j2, m2, J, M;
-  std::cout<<"Clebsh-Gordon coef. Enter in order: <j1 m1, j2 m2 | J M>:\n";
-  std::cin>>j1>>m1>>j2>>m2>>J>>M;
-  double cgc = WIG::cg(j1,m1,j2,m2,J,M);
-  printf("<%.1f %.1f, %.1f %.1f| %.1f %.1f> = %f\n",j1,m1,j2,m2,J,M,cgc);
-  return 0;
+void instructions()
+{
+  std::cout<<"\nI don't understand that input.\n";
+  std::cout<<"Enter 3,6,8j/CG symbols like:\n\n";
+  std::cout<<"  3j:  \'(j1,j2,j3,m1,m2,m3)\',\n";
+  std::cout<<"  6j:  \'{j1,j2,j3,j4,j5,j6}\',\n";
+  std::cout<<"  9j:  \'{j1,j2,j3,j4,j5,j6,j7,j8,j9}\',\n or \n";
+  std::cout<<"  Clebschscgh Gordon:   \'<j1,m1, j2,m2 | J,M>\'\n\n";
+  std::cout<<"(including the quote marks!)\n"
+    <<"You can use any symbol in place of the commas (incl space).\n\n";
 }
 
-int case3j(){
-  double j1, j2, j3, m1, m2, m3;
-  std::cout<<"3j symbol. Enter in order: j1 j2 j3 m1 m2 m3:\n";
-  std::cin>>j1>>j2>>j3>>m1>>m2>>m3;
-  double cgc = WIG::threej(j1,j2,j3,m1,m2,m3);
-  printf("(%5.1f %5.1f %5.1f) = %f\n",j1,j2,j3,cgc);
-  printf("(%5.1f %5.1f %5.1f)\n",m1,m2,m3);
-  return 0;
-}
-
-int case6j(){
-  double j1, j2, j3, j4, j5, j6;
-  std::cout<<"6j symbol. Enter in order: j1 j2 j3 j4 j5 j6:\n";
-  std::cin>>j1>>j2>>j3>>j4>>j5>>j6;
-  double cgc = WIG::sixj(j1,j2,j3,j4,j5,j6);
-  printf("{%5.1f %5.1f %5.1f} = %f\n",j1,j2,j3,cgc);
-  printf("{%5.1f %5.1f %5.1f}\n",j4,j5,j6);
-  return 0;
-}
-
-int case9j(){
-  double j1, j2, j3, j4, j5, j6, j7, j8, j9;
-  std::cout<<"9j symbol. Enter in order: j1 j2 j3 j4 j5 j6 j7 j8 j9:\n";
-  std::cin>>j1>>j2>>j3>>j4>>j5>>j6>>j7>>j8>>j9;
-  double cgc = WIG::ninej(j1,j2,j3,j4,j5,j6,j7,j8,j9);
-  printf("{%5.1f %5.1f %5.1f}\n",j1,j2,j3);
-  printf("{%5.1f %5.1f %5.1f} = %f\n",j4,j5,j6,cgc);
-  printf("{%5.1f %5.1f %5.1f}\n",j7,j8,j9);
-  return 0;
-}
 
 int main(int num_in, char* argv[]){
 
-  BEG:
 
-  int i_type=-1;
-  if(num_in>1){
-    i_type = std::stoi(argv[1]);
-  }else{
-    std::cout<<"Which symbol do you want? 3,6,9? (0 for CG)\n";
-    std::cin>>i_type;
+  if(num_in <= 1){
+    instructions();
+    return 1;
   }
 
-  if(i_type==0){
-    return caseCG();
-  }else if(i_type==3){
-    return case3j();
-  }else if(i_type==6){
-    return case6j();
-  }else if(i_type==9){
-    return case9j();
-  }else{
-    std::cout<<i_type<<" is not valid. Enter 0, 3, 6, or 9.\n";
-    goto BEG;
-    // return 0;
+  std::string input = argv[1];
+
+  std::istringstream stream(input);
+  std::vector<double> v;
+  while(true) {
+    double j;
+    if(stream >> j) {
+      v.push_back(j);
+    } else if (stream.eof()) {
+      break; // We're done
+    } else {
+      // We've hit a non-int
+      stream.clear(); // clear the error flags
+      stream.ignore(); // ignore everything till the delimeter
+    }
   }
+
+  if(input.substr(0,1)=="("){
+    if(v.size()!=6){
+      instructions();
+      return 1;
+    }
+    double cgc = WIG::threej(v[0],v[1],v[2],v[3],v[4],v[5]);
+    std::cout<<"3j symbol:\n";
+    printf("(%4.1f%4.1f%4.1f)\n",v[0],v[1],v[2]);
+    printf("(%4.1f%4.1f%4.1f) = %.6f\n",v[3],v[4],v[5],cgc);
+  }else if(input.substr(0,1)=="<"){
+    if(v.size()!=6){
+      instructions();
+      return 1;
+    }
+    std::cout<<"Clebschscgh Gordon coeficient:\n";
+    double cgc = WIG::cg(v[0],v[1],v[2],v[3],v[4],v[5]);
+    printf("<%.1f %.1f, %.1f %.1f| %.1f %.1f> = %f\n",
+      v[0],v[1],v[2],v[3],v[4],v[5],cgc);
+  }else if(input.substr(0,1)=="{"){
+    if(v.size()==6){
+      std::cout<<"6j symbol:\n";
+      double cgc = WIG::sixj(v[0],v[1],v[2],v[3],v[4],v[5]);
+      printf("{%4.1f%4.1f%4.1f}\n",v[0],v[1],v[2]);
+      printf("{%4.1f%4.1f%4.1f} = %.6f\n",v[3],v[4],v[5],cgc);
+    }else if(v.size()==9){
+      std::cout<<"9j symbol:\n";
+      double cgc = WIG::ninej(v[0],v[1],v[2],v[3],v[4],v[5],v[6],v[7],v[8]);
+      printf("{%4.1f%4.1f%4.1f}\n",v[0],v[1],v[2]);
+      printf("{%4.1f%4.1f%4.1f} = %.6f\n",v[3],v[4],v[5],cgc);
+      printf("{%4.1f%4.1f%4.1f}\n",v[6],v[7],v[8]);
+    }
+    else{
+      instructions();
+    }
+  }
+
 
   return 0;
 }
