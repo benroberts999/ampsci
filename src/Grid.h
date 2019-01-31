608d5e10a88f0bf7aff961b3c8b83385f9d772b7
B M Roberts <benroberts999@gmail.com>
2019-01-31 18:41:56 +0100

    starting update grid

--- src/Grid.h
+++ src/Grid.h
@@ -2,11 +2,34 @@
 
 enum class GridType {loglinear, logarithmic, linear};
 
+/*
+
+Use the keyword 'static' to declare the method:
+
+static int MyMethod( int * a, int * b );
+Then you can call the method without an instance like so:
+
+int one = 1;
+int two = 2;
+
+MyClass::MyMethod( &two, &one );
+'static' methods are functions which only use the class as a namespace, and do not require an instance.
+
+The, grid MUST be called with ngp only (no h)!
+But I can use h to generate ngp outside class if I want!
+
+
+*/
+
+
+
 //******************************************************************************
 struct Grid{
 
-  Grid();
+  Grid(double in_r0, double in_rmax, int in_ngp,
+    GridType gridtype = GridType::loglinear, double in_b=4.);
 
+  // const GridType gridtype;
   const double r0;
   const double rmax;
   const int ngp;
@@ -16,16 +39,42 @@ struct Grid{
   std::vector<double> drdu;
   std::vector<double> drduor;
 
+  void printGrid();
 
 private:
-  void exponentialRadialGrid(int ngp_in, double r0, double rmax);
-  void logLinearRadialGrid(int ngp_in, double r0, double rmax, double b=4.);
-  void logLinearRadialGrid(double in_h, double r0, double rmax, double b=4.);
+  void formLogLinearGrid();
+  void formLogGrid();
+  void formLinearGrid();
 
   double calcdu(GridType gridtype, double in_r0, double in_rmax, int in_ngp,
     double b);
-
 };
+
+//******************************************************************************
+Grid::Grid(double in_r0, double in_rmax, int in_ngp, GridType gridtype,
+  double b)
+  : r0(in_r0), rmax(in_rmax), ngp(in_ngp),
+  du(calcdu(in_ngp, gridtype, b))
+{
+
+  r.clear();
+  drdu.clear();
+  drduor.clear();
+
+  r.reserve(ngp);
+  drdu.reserve(ngp);
+  drduor.reserve(ngp);
+
+  switch(gridtype){
+    case GridType::loglinear : formLogLinearGrid(); break;
+    case GridType::logarithmic : formLogGrid(); break;
+    case GridType::loglinear : logLinearRadialGrid(); break;
+    default : std::cerr<<"\n FAIL 49 in Grid: no grid type?\n";
+  }
+
+}
+
+
 
 
 //******************************************************************************
@@ -38,26 +87,18 @@ double calcdu(GridType gridtype, double in_r0, double in_rmax, int in_ngp,
   }
 }
 
+
 //******************************************************************************
-Grid::Grid(GridType gridtype, double in_r0, double in_rmax, int in_ngp,
-  double b)
-  : r0(in_r0), rmax(in_rmax), ngp(in_ngp),
-  du(calcdu(gridtype, in_r0, in_rmax, in_ngp, b))
-{
-
-  r.clear();
-  drdu.clear();
-  drduor.clear();
-
-  r.reserve(ngp);
-  drdu.reserve(ngp);
-  drduor.reserve(ngp);
-
-}
-
-
-
 void Grid::form_loglinear_grid()
+/*
+Roughly, grid is logarithmically spaced below r=b, and linear above.
+Definition:
+  dr/du = r/(b+r)
+  => u_0 = r0 + b*ln(r0)
+  du = (in_rmax-in_r0+b*log(in_rmax/in_r0))/(in_ngp-1)
+  du is constant (step-size for uniformly spaced grid)
+Typically (and by default), b = 4 (unit units/bohr radius)
+*/
 {
 
   //initial points:
@@ -65,12 +106,8 @@ void Grid::form_loglinear_grid()
   drduor.push_back(1./(b+r0));
   drdu.push_back(drduor[0]*r0);
 
-  /*
-  r(u[i]) = r0 ...... put equations!
-  */
-
-  // Use method from Dzuba code to calculate r grid
-  double u = r0 + b*log(r0); //t is linear/uniform grid
+  // Use iterative method from Dzuba code to calculate r grid
+  double u = r0 + b*log(r0);
   for (int i=1; i<ngp; i++) {
     u += du;
     double r_tmp = r[i-1];
@@ -90,25 +127,3 @@ void Grid::form_loglinear_grid()
     drdu.push_back(drduor[i]*r_tmp);
   }
 }
-
-/*
-// Lattice coordinate x = rmin + ih,
-//      y = x + beta * log(rmin)
-double y = rmin + beta*log(rmin) + h*double(i);
-double r, rold;
-
-if(beta <= y)
-    r = y;
-else
-    r = exp(y/beta);
-
-// Solve using Newton's method f(r) == 0, where
-//    f(r) = r + beta*log(r/rmin) - x
-//         = r + beta*log(r) - y
-do
-{   rold = r;
-    r = r - (r + beta*log(r) - y)/(1. + beta/r);
-    if(r <= 0.0)
-        r = rold/2.0;
-} while(fabs(r - rold)/r > 1.e-13);
-*/
