4d6d5f49a42ee2dc9c34c8ca0de2ec568181a177
BM Roberts <b.roberts@uq.edu.au>
2019-06-06 16:27:30 +1000

    adds <ka||C^k||kb> to wigner

--- src/Wigner_369j.hpp
+++ src/Wigner_369j.hpp
@@ -19,10 +19,15 @@ integer angular momentum (l).
 */
 
 //******************************************************************************
+inline int l_k(int ka) { return (ka > 0) ? ka : -ka - 1; }
+inline int twoj_k(int ka) { return (ka > 0) ? 2 * ka - 1 : -2 * ka - 1; }
+inline double j_k(int ka) {
+  return (ka > 0) ? double(ka) - 0.5 : double(-ka) - 0.5;
+}
+
+//******************************************************************************
 inline int parity(int la, int lb, int k)
-/*
-Parity rule. Returns 1 only if la+lb+k is even
-*/
+// Parity rule. Returns 1 only if la+lb+k is even
 {
   if ((la + lb + k) % 2 == 0)
     return 1;
@@ -31,9 +36,7 @@ Parity rule. Returns 1 only if la+lb+k is even
 
 //******************************************************************************
 inline int triangle(double j1, double j2, double J)
-/*
-Triangle rule. XXX NOT TESTED XXX
-*/
+// Triangle rule.
 {
   int two_j1 = (int)round(2 * j1);
   int two_j2 = (int)round(2 * j2);
@@ -46,13 +49,11 @@ Triangle rule. XXX NOT TESTED XXX
 //******************************************************************************
 inline double threej(double j1, double j2, double j3, double m1, double m2,
                      double m3)
-/*
-Calculates wigner 3j symbol:
-  (j1 j2 j3)
-  (m1 m2 m3)
-Note: this function takes DOUBLE values.
-Works for l and j (integer and half-integer)
-*/
+// Calculates wigner 3j symbol:
+//   (j1 j2 j3)
+//   (m1 m2 m3)
+// Note: this function takes DOUBLE values.
+// Works for l and j (integer and half-integer)
 {
   int two_j1 = (int)round(2 * j1);
   int two_j2 = (int)round(2 * j2);
@@ -65,12 +66,11 @@ Works for l and j (integer and half-integer)
 
 //------------------------------------------------------------------------------
 inline double threej_1(int j1, int j2, int j3, int m1, int m2, int m3)
-/*
-Calculates wigner 3j symbol:
-  (j1 j2 j3)
-  (m1 m2 m3)
-Note: this function takes INTEGER values, only works for l (not half-integer j)!
-*/
+// Calculates wigner 3j symbol:
+//   (j1 j2 j3)
+//   (m1 m2 m3)
+// Note: this function takes INTEGER values, only works for l (not half-integer
+// j)!
 {
   return gsl_sf_coupling_3j(2 * j1, 2 * j2, 2 * j3, 2 * m1, 2 * m2, 2 * m3);
 }
@@ -78,26 +78,22 @@ Note: this function takes INTEGER values, only works for l (not half-integer j)!
 //------------------------------------------------------------------------------
 inline double threej_2(int two_j1, int two_j2, int two_j3, int two_m1,
                        int two_m2, int two_m3)
-/*
-Calculates wigner 3j symbol:
-  (j1 j2 j3)
-  (m1 m2 m3)
-Note: this function takes INTEGER values, that have already multiplied by 2!
-Works for l and j (integer and half-integer)
-*/
+// Calculates wigner 3j symbol:
+//   (j1 j2 j3)
+//   (m1 m2 m3)
+// Note: this function takes INTEGER values, that have already multiplied by 2!
+// Works for l and j (integer and half-integer)
 {
   return gsl_sf_coupling_3j(two_j1, two_j2, two_j3, two_m1, two_m2, two_m3);
 }
 
 //******************************************************************************
 inline double cg(double j1, double m1, double j2, double m2, double J, double M)
-/*
-<j1 m1, j2 m2 | J M> = (-1)^(j1-j2+M) * Sqrt(2J+1) * (j1 j2  J)
-.                                                    (m1 m2 -M)
-(Last term is 3j symbol)
-Note: this function takes DOUBLE values.
-Works for l and j (integer and half-integer)
-*/
+// <j1 m1, j2 m2 | J M> = (-1)^(j1-j2+M) * Sqrt(2J+1) * (j1 j2  J)
+// .                                                    (m1 m2 -M)
+// (Last term is 3j symbol)
+// Note: this function takes DOUBLE values.
+// Works for l and j (integer and half-integer)
 {
   int two_j1 = (int)round(2 * j1);
   int two_j2 = (int)round(2 * j2);
@@ -114,13 +110,12 @@ Works for l and j (integer and half-integer)
 
 //------------------------------------------------------------------------------
 inline double cg_1(int j1, int m1, int j2, int m2, int J, int M)
-/*
-Calculates Clebsh-Gordon coeficient:
-<j1 m1, j2 m2 | J M> = (-1)^(j1-j2+M) * Sqrt(2J+1) * (j1 j2  J)
-.                                                    (m1 m2 -M)
-(Last term is 3j symbol)
-Note: this function takes INTEGER values, only works for l (not half-integer j)!
-*/
+// Calculates Clebsh-Gordon coeficient:
+// <j1 m1, j2 m2 | J M> = (-1)^(j1-j2+M) * Sqrt(2J+1) * (j1 j2  J)
+// .                                                    (m1 m2 -M)
+// (Last term is 3j symbol)
+// Note: this function takes INTEGER values, only works for l (not half-integer
+// j)!
 {
   int sign = -1;
   if ((j1 - j2 + M) % 2 == 0)
@@ -132,13 +127,11 @@ Note: this function takes INTEGER values, only works for l (not half-integer j)!
 //------------------------------------------------------------------------------
 inline double cg_2(int two_j1, int two_m1, int two_j2, int two_m2, int two_J,
                    int two_M)
-/*
-<j1 m1, j2 m2 | J M> = (-1)^(j1-j2+M) * Sqrt(2J+1) * (j1 j2  J)
-.                                                    (m1 m2 -M)
-(Last term is 3j symbol)
-Note: this function takes INTEGER values, that have already multiplied by 2!
-Works for l and j (integer and half-integer)
-*/
+// <j1 m1, j2 m2 | J M> = (-1)^(j1-j2+M) * Sqrt(2J+1) * (j1 j2  J)
+// .                                                    (m1 m2 -M)
+// (Last term is 3j symbol)
+// Note: this function takes INTEGER values, that have already multiplied by 2!
+// Works for l and j (integer and half-integer)
 {
   int sign = -1;
   if ((two_j1 - two_j2 + two_M) % 4 == 0)
@@ -150,13 +143,11 @@ Works for l and j (integer and half-integer)
 //******************************************************************************
 inline double sixj(double j1, double j2, double j3, double j4, double j5,
                    double j6)
-/*
-Calculates wigner 6j symbol:
-  {j1 j2 j3}
-  {j4 j5 j6}
-Note: this function takes DOUBLE values.
-Works for l and j (integer and half-integer)
-*/
+// Calculates wigner 6j symbol:
+//   {j1 j2 j3}
+//   {j4 j5 j6}
+// Note: this function takes DOUBLE values.
+// Works for l and j (integer and half-integer)
 {
   int two_j1 = (int)round(2 * j1);
   int two_j2 = (int)round(2 * j2);
@@ -169,12 +160,11 @@ Works for l and j (integer and half-integer)
 
 //------------------------------------------------------------------------------
 inline double sixj_1(int j1, int j2, int j3, int j4, int j5, int j6)
-/*
-Calculates wigner 6j symbol:
-  {j1 j2 j3}
-  {j4 j5 j6}
-Note: this function takes INTEGER values, only works for l (not half-integer j)!
-*/
+// Calculates wigner 6j symbol:
+//   {j1 j2 j3}
+//   {j4 j5 j6}
+// Note: this function takes INTEGER values, only works for l (not half-integer
+// j)!
 {
   return gsl_sf_coupling_6j(2 * j1, 2 * j2, 2 * j3, 2 * j4, 2 * j5, 2 * j6);
 }
@@ -182,13 +172,11 @@ Note: this function takes INTEGER values, only works for l (not half-integer j)!
 //------------------------------------------------------------------------------
 inline double sixj_2(int two_j1, int two_j2, int two_j3, int two_j4, int two_j5,
                      int two_j6)
-/*
-Calculates wigner 6j symbol:
-  {j1 j2 j3}
-  {j4 j5 j6}
-Note: this function takes INTEGER values, that have already multiplied by 2!
-Works for l and j (integer and half-integer)
-*/
+// Calculates wigner 6j symbol:
+//   {j1 j2 j3}
+//   {j4 j5 j6}
+// Note: this function takes INTEGER values, that have already multiplied by 2!
+// Works for l and j (integer and half-integer)
 {
   return gsl_sf_coupling_6j(two_j1, two_j2, two_j3, two_j4, two_j5, two_j6);
 }
@@ -196,14 +184,12 @@ Works for l and j (integer and half-integer)
 //******************************************************************************
 inline double ninej(double j1, double j2, double j3, double j4, double j5,
                     double j6, double j7, double j8, double j9)
-/*
-Calculates wigner 9j symbol:
-  {j1 j2 j3}
-  {j4 j5 j6}
-  {j7 j8 j9}
-Note: this function takes DOUBLE values.
-Works for l and j (integer and half-integer)
-*/
+// Calculates wigner 9j symbol:
+//   {j1 j2 j3}
+//   {j4 j5 j6}
+//   {j7 j8 j9}
+// Note: this function takes DOUBLE values.
+// Works for l and j (integer and half-integer)
 {
   int two_j1 = (int)round(2 * j1);
   int two_j2 = (int)round(2 * j2);
@@ -221,13 +207,12 @@ Works for l and j (integer and half-integer)
 //------------------------------------------------------------------------------
 inline double ninej_1(int j1, int j2, int j3, int j4, int j5, int j6, int j7,
                       int j8, int j9)
-/*
-Calculates wigner 9j symbol:
-  {j1 j2 j3}
-  {j4 j5 j6}
-  {j7 j8 j9}
-Note: this function takes INTEGER values, only works for l (not half-integer j)!
-*/
+// Calculates wigner 9j symbol:
+//   {j1 j2 j3}
+//   {j4 j5 j6}
+//   {j7 j8 j9}
+// Note: this function takes INTEGER values, only works for l (not half-integer
+// j)!
 {
   return gsl_sf_coupling_9j(2 * j1, 2 * j2, 2 * j3, 2 * j4, 2 * j5, 2 * j6,
                             2 * j7, 2 * j8, 2 * j9);
@@ -237,16 +222,32 @@ Note: this function takes INTEGER values, only works for l (not half-integer j)!
 inline double ninej_2(int two_j1, int two_j2, int two_j3, int two_j4,
                       int two_j5, int two_j6, int two_j7, int two_j8,
                       int two_j9)
-/*
-Calculates wigner 9j symbol:
-  {j1 j2 j3}
-  {j4 j5 j6}
-  {j7 j8 j9}
-Note: this function takes INTEGER values, that have already multiplied by 2!
-Works for l and j (integer and half-integer)*/
+// Calculates wigner 9j symbol:
+//   {j1 j2 j3}
+//   {j4 j5 j6}
+//   {j7 j8 j9}
+// Note: this function takes INTEGER values, that have already multiplied by 2!
+// Works for l and j (integer and half-integer)
 {
   return gsl_sf_coupling_9j(two_j1, two_j2, two_j3, two_j4, two_j5, two_j6,
                             two_j7, two_j8, two_j9);
+}
+
+//******************************************************************************
+inline double Ck_kk(int k, int ka, int kb)
+// Reduced (relativistic) angular ME:
+// <ka||C^k||kb> = (-1)^(ja+1/2) * 3js(ja jb k, -1/2 1/2 0) * Pi
+// Note: takes in kappa! (not j!)
+{
+  if (parity(l_k(ka), l_k(kb), k) == 0) {
+    return 0;
+  }
+  auto two_ja = twoj_k(ka);
+  auto two_jb = twoj_k(kb);
+  auto sign = ((two_ja + 1) / 2 % 2 == 0) ? 1 : -1;
+  auto f = sqrt((two_ja + 1) * (two_jb + 1));
+  auto g = gsl_sf_coupling_3j(two_ja, two_jb, 2 * k, -1, 1, 0);
+  return sign * f * g;
 }
 
 } // namespace Wigner
