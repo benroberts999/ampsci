0ba9f19169ed6e902a5efb57e5803fc7135b401c
BM Roberts <b.roberts@uq.edu.au>
2019-06-07 12:17:47 +1000

    Starting Coulomb Class

--- src/CoulombFunctions.cpp
+++ src/CoulombFunctions.cpp
@@ -3,10 +3,11 @@
 #include "NumCalc_quadIntegrate.hpp"
 #include <vector>
 
-namespace Coulomb {
+// namespace Coulomb
 
-void calculate_v_abk(const DiracSpinor &phi_a, const DiracSpinor &phi_b,
-                     const int k, std::vector<double> &vabk)
+void Coulomb::calculate_v_abk(const DiracSpinor &phi_a,
+                              const DiracSpinor &phi_b, const int k,
+                              std::vector<double> &vabk)
 // Calculalates v^k_ab screening function.
 // Note: should only call for a>=b, and for k's with non-zero angular coefs
 // (nothing bad will happen othersie, but no point!)
@@ -54,4 +55,4 @@ void calculate_v_abk(const DiracSpinor &phi_a, const DiracSpinor &phi_b,
   }
 }
 
-} // namespace Coulomb
+// } // namespace Coulomb
