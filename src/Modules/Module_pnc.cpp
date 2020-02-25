9d7357e0c1eddddb935aa764d7ebe3a21a657b0a
BM Roberts <b.roberts@uq.edu.au>
2020-02-25 19:15:53 +1000

    Fixes minor typo in pnc module introduced last commit or so

--- src/Modules/Module_pnc.cpp
+++ src/Modules/Module_pnc.cpp
@@ -109,14 +109,14 @@ void Module_testPNC(const UserInputBlock &input, const Wavefunction &wf) {
                     (aB.en - np.en);
       double pnc2 = c01 * hpnc.reducedME(aA, np) * he1.reducedME(np, aB) /
                     (aA.en - np.en);
-      if (np.n <= main_n)
-        // printf("%7s, pnc= %12.5e + %12.5e = %12.5e\n", np.symbol().c_str(),
-        //        pnc1, pnc2, pnc1 + pnc2);
-        pnc += pnc1 + pnc2;
+      // if (np.n <= main_n)
+      // printf("%7s, pnc= %12.5e + %12.5e = %12.5e\n", np.symbol().c_str(),
+      //        pnc1, pnc2, pnc1 + pnc2);
+      pnc += pnc1 + pnc2;
       if (np.n <= ncore)
-        core = pnc;
+        core += pnc1 + pnc2;
       if (np.n <= main_n && np.n > ncore)
-        main = pnc - core;
+        main += pnc1 + pnc2;
       if (np.n == main_n)
         main_ok = true;
       if (np.n > max_n_main)
