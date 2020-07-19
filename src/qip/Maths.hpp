c68cb445b71a6eb03e3456028bc997e45cf514e7
Ben Roberts <b.roberts@uq.edu.au>
2020-07-19 13:08:02 +1000

    qip

--- src/qip/Maths.hpp
+++ src/qip/Maths.hpp
@@ -31,20 +31,60 @@ template <typename T, typename... Args> T min_abs(T first, Args... rest) {
 //******************************************************************************
 //! x^n for integer n (n compile-time template parameter)
 template <int n, typename T> constexpr auto pow(T x) {
-  static_assert(
-      std::is_arithmetic_v<T>,
-      "In compare(std::vector<T>, std::vector<T>), T must be arithmetic");
+  static_assert(std::is_arithmetic_v<T>, "In pow(T x), T must be arithmetic");
   // Returns double for inverse powers, T otherwise
   if constexpr (n < 0) {
     return double(1.0) / pow<-n>(x);
   } else if constexpr (n == 0) {
     (void)x; // 'x' unused in this branch
     return static_cast<T>(1);
-    // } else if constexpr (n == 1) {
-    //   return x;
+  } else if constexpr (n == 1) {
+    return x;
   } else {
     return x * pow<n - 1>(x);
   }
+}
+
+//! x^n for integer n (runtime n)
+template <typename T> constexpr T pow(T x, int n) {
+  static_assert(std::is_floating_point_v<T>,
+                "In pow(T x, int n), T must be floating point");
+  if (n < 0)
+    return static_cast<T>(1) / pow<T>(x, -n);
+  T result = static_cast<T>(1);
+  for (int i = 0; i < n; ++i) {
+    result *= x;
+  }
+  return result;
+}
+
+//******************************************************************************
+//! Returns sign of value
+template <typename T> constexpr int sign(T value) {
+  static_assert(std::is_arithmetic_v<T>,
+                "In sign(T value), T must be arithmetic");
+  return (T(0) < value) - (value < T(0));
+}
+
+//! Clips value to between -max <= value <= max
+//! clip(x,max) : |x| > max, ret max; if |x|<-max, -max; else x
+template <typename T> constexpr T clip(T value, T max_abs) {
+  static_assert(std::is_arithmetic_v<T>,
+                "In clip(T value, T max_abs), T must be arithmetic");
+  if (value > max_abs)
+    return max_abs;
+  if (value < -max_abs)
+    return -max_abs;
+  return value;
+}
+
+//! Sets values |v|<min to zero; if |v|>=min, returns v
+template <typename T> constexpr T chop(T value, T min_abs) {
+  static_assert(std::is_arithmetic_v<T>,
+                "In clip(T value, T max_abs), T must be arithmetic");
+  if (std::abs(value) < min_abs)
+    return static_cast<T>(0);
+  return value;
 }
 
 } // namespace qip
