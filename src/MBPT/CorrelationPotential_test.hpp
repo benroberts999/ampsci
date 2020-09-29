972208d6676409a653ffd5ca08109b94c6e71175
BM Roberts <b.roberts@uq.edu.au>
2020-09-29 18:01:55 +1000

    Adds All-orders sigma unit test

--- src/MBPT/CorrelationPotential_test.hpp
+++ src/MBPT/CorrelationPotential_test.hpp
@@ -15,7 +15,7 @@ namespace UnitTest {
 bool CorrelationPotential(std::ostream &obuff) {
   bool pass = true;
 
-  { // Compare with  K. Beloy and A. Derevianko,
+  if (false) { // Compare with  K. Beloy and A. Derevianko,
     // Comput. Phys. Commun. 179, 310 (2008).
     Wavefunction wf({4000, 1.0e-6, 100.0, 0.33 * 100.0, "loglinear", -1.0},
                     {"Cs", -1, "Fermi", -1.0, -1.0}, 1.0);
@@ -63,7 +63,7 @@ bool CorrelationPotential(std::ostream &obuff) {
   }
 
   //****************************************************************************
-  { // Compare Dzuba, only using up to l=4 for splines
+  if (false) { // Compare Dzuba, only using up to l=4 for splines
     // Note: Works pretty well up to f states (not sure if difference is ok)
     auto dzuba_g = std::vector{
         -0.0195938,  -0.00399679, -0.00770113, -0.00682331, -0.00214125,
@@ -96,7 +96,7 @@ bool CorrelationPotential(std::ostream &obuff) {
   }
   //
 
-  { // Compare Dzuba, using up to l=6 for splines
+  if (false) { // Compare Dzuba, using up to l=6 for splines
     auto dzuba_i = std::vector{
         -0.02013813, -0.00410942, -0.00792483, -0.00702407, -0.00220878,
         -0.00199737, -0.01551449, -0.01466935, -0.00035253, -0.00035234};
@@ -128,6 +128,52 @@ bool CorrelationPotential(std::ostream &obuff) {
 
     auto [eps, at] = qip::compare_eps(dzuba_i, de);
     pass &= qip::check_value(&obuff, "Sigma2 Cs (spdfghi)", eps, 0.0, 0.01);
+  }
+
+  { // Compare Dzuba, All-order sigma
+    auto dzuba_i =
+        std::vector{-0.14332871, -0.05844404, -0.09244689, -0.08985968,
+                    -0.04392404, -0.04309476, -0.07812666, -0.07759564};
+    std::sort(begin(dzuba_i), end(dzuba_i)); // sort: don't depend on order
+
+    Wavefunction wf({4000, 1.0e-6, 120.0, 0.33 * 120.0, "loglinear", -1.0},
+                    {"Cs", -1, "Fermi", -1.0, -1.0}, 1.0);
+    wf.hartreeFockCore("HartreeFock", 0.0, "[Xe]");
+    wf.hartreeFockValence("7sp5d");
+    wf.formBasis({"35spdfghi", 40, 9, 0.0, 1.0e-6, 40.0, false});
+
+    const auto n_min_core = 3;
+    const auto rmin = 1.0e-4;
+    const auto rmax = 30.0;
+    const auto stride =
+        int(wf.rgrid->getIndex(30.0) - wf.rgrid->getIndex(1.0e-4)) / 150;
+
+    const auto omre = -std::abs(0.33 * wf.energy_gap());
+    const double w0 = 0.01;
+    const double wratio = 1.5;
+    const auto lmax = 6;
+
+    const std::vector fk{0.71, 0.589, 0.84, 0.885, 0.95, 0.976, 0.991};
+    // const std::vector fk{0.72, 0.62, 0.83, 0.89, 0.94, 1.0};
+
+    // wf.formSigma(3, true, 1.0e-4, 30.0, 14 /*stride*/);
+    wf.formSigma(n_min_core, true, rmin, rmax, stride, false, {}, fk, "", true,
+                 true, true, lmax, false, false, omre, w0, wratio);
+
+    wf.hartreeFockBrueckner();
+
+    std::vector<double> br;
+    for (const auto &Fv : wf.valence) {
+      br.push_back(Fv.en);
+    }
+    std::sort(begin(br), end(br)); // sort: don't depend on order
+
+    for (auto i = 0ul; i < dzuba_i.size(); ++i) {
+      std::cout << dzuba_i[i] << " " << br[i] << "\n";
+    }
+
+    auto [eps, at] = qip::compare_eps(dzuba_i, br);
+    pass &= qip::check_value(&obuff, "Sigma all-orders Cs", eps, 0.0, 5e-04);
   }
 
   return pass;
