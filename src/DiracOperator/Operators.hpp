4e2facb67fd8e699997b9d152ce14f99835651bd
BM Roberts <b.roberts@uq.edu.au>
2020-10-30 10:31:29 +1000

    Fixes issue for static M1 operator

--- src/DiracOperator/Operators.hpp
+++ src/DiracOperator/Operators.hpp
@@ -121,16 +121,15 @@ private:
 //! Magnetic dipole operator: <a||M1||b>
 /*! @details
 \f[ <a||M1||b> = R (k_a + k_b) <-k_a||C^1||k_b> \f]
-\f[R = \frac{3}{\alpha^2\omega} \int (f_ag_b+g_af_b) j_1(kr) \, dr\f]
+\f[R = \frac{-3}{\alpha^2\omega} \int (f_ag_b+g_af_b) j_1(kr) \, dr\f]
 \f$ k = \omega/c = \omega*\alpha \f$.
-Check sign?
+Negative sign puts into units |mu_B|
 */
 class M1 final : public TensorOperator {
 public:
-  // XXX Check sign!
   M1(const Grid &gr, const double alpha, const double omega = 0.0)
       : TensorOperator(1, Parity::even, +0.0, {}, 0, Realness::real, true),
-        m_gr(&gr),
+        m_r(gr.r),
         m_alpha(alpha) {
     updateFrequency(omega);
   }
@@ -138,7 +137,7 @@ public:
   M1(const M1 &) = default;
   ~M1() = default;
   std::string name() const override final { return std::string("M1"); }
-  std::string units() const override final { return std::string("mu_B"); }
+  std::string units() const override final { return std::string("|mu_B|"); }
 
   double angularF(const int ka, const int kb) const override final {
     return (ka + kb) * Angular::Ck_kk(1, -ka, kb);
@@ -149,18 +148,18 @@ public:
   double angularCgf(int, int) const override final { return 1.0; }
 
   void updateFrequency(const double omega) override final {
-    // XXX Check sign!
-    if (std::abs(omega) > 0) {
-      m_constant = +3.0 / (m_alpha * m_alpha * omega);
-      m_vec = SphericalBessel::fillBesselVec_kr(1, omega * m_alpha, m_gr->r);
+    if (std::abs(omega) > 1.0e-10) {
+      m_constant = -3.0 / (m_alpha * m_alpha * omega);
+      m_vec = SphericalBessel::fillBesselVec_kr(1, omega * m_alpha, m_r);
     } else {
-      m_constant = +1.0 / (m_alpha);
-      m_vec.clear();
+      // j1(kr) -> (r*k)/3, for k<<1
+      m_constant = -1.0 / (m_alpha);
+      m_vec = m_r;
     }
   }
 
 private:
-  const Grid *const m_gr;
+  const std::vector<double> m_r; // store radial vector (copy)
   const double m_alpha;
 };
 
