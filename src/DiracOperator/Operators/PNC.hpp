a5162fc157760fc25bf51c9c1e1078a0146044b2
BM Roberts <b.roberts@uq.edu.au>
2021-07-27 10:57:39 +1000

    Update sign for pnc operator

--- src/DiracOperator/Operators/PNC.hpp
+++ src/DiracOperator/Operators/PNC.hpp
@@ -7,9 +7,11 @@ namespace DiracOperator {
 //! @brief Nuclear-spin independent PNC operator (Qw)
 /*! @details
 \f[
-h_{PNCnsi} = \frac{G_F \, Q_W}{2\sqrt{2}} \, \rho(r) \, \gamma_5.
+h_{PNCnsi} = \frac{G_F \, Q_W}{2\sqrt{2}} \, \rho(r) \, \gamma^5.
 \f]
-  - Check -ve sign?
+\f[
+h_{PNCnsi} = -\frac{G_F \, Q_W}{2\sqrt{2}} \, \rho(r) \, \gamma_5.
+\f]
   - Ouput is in units of (Qw * 1.e-11.) by default. To get (Qw/-N), multiply
   by (-N) [can go into optional 'factor']
 
@@ -22,7 +24,7 @@ public:
          const std::string &in_units = "iQw*e-11")
       : ScalarOperator(Parity::odd, factor * PhysConst::GFe11 / std::sqrt(8.0),
                        Nuclear::fermiNuclearDensity_tcN(t, c, 1, rgrid),
-                       {0, 1, -1, 0}, 0, Realness::imaginary),
+                       {0, -1, 1, 0}, 0, Realness::imaginary),
         m_unit(in_units) {}
   std::string name() const override final { return "pnc-nsi"; }
   std::string units() const override final { return m_unit; }
