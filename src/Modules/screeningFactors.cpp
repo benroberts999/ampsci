f91d6322ddddcc78b2f7f8361b844eb96c48936b
BM Roberts <b.roberts@uq.edu.au>
2021-12-10 12:26:37 +1000

    Update to calc screening factors: hole-particle

--- src/Modules/screeningFactors.cpp
+++ src/Modules/screeningFactors.cpp
@@ -5,6 +5,7 @@
 #include "Modules/testFeynman.hpp"
 #include "Wavefunction/DiracSpinor.hpp"
 #include "Wavefunction/Wavefunction.hpp"
+#include "qip/Vector.hpp"
 #include <iostream>
 #include <vector>
 
@@ -19,7 +20,7 @@ void screeningFactors(const IO::InputBlock &input, const Wavefunction &wf) {
   const auto omre = -std::abs(0.33 * wf.energy_gap());
 
   const auto stride =
-      (wf.rgrid->getIndex(30.0) - wf.rgrid->getIndex(1.0e-4)) / 150;
+      (wf.rgrid->getIndex(30.0) - wf.rgrid->getIndex(1.0e-3)) / 100;
 
   double w0 = 0.01;
   double wratio = 1.5;
@@ -29,18 +30,45 @@ void screeningFactors(const IO::InputBlock &input, const Wavefunction &wf) {
 
   const auto include_G = false;
 
-  const MBPT::Sigma_params sigp_X{MBPT::Method::Feynman,
-                                  n_min_core,
-                                  include_G,
-                                  max_l_excited,
-                                  false,
-                                  false,
-                                  omre,
-                                  w0,
-                                  wratio,
-                                  true,
-                                  false,
-                                  {}};
+  const MBPT::Sigma_params sigp_scr{MBPT::Method::Feynman,
+                                    n_min_core,
+                                    include_G,
+                                    max_l_excited,
+                                    false,
+                                    false,
+                                    omre,
+                                    w0,
+                                    wratio,
+                                    true,  // screening
+                                    false, // hole-particle
+                                    {}};
+
+  const MBPT::Sigma_params sigp_hp{MBPT::Method::Feynman,
+                                   n_min_core,
+                                   include_G,
+                                   max_l_excited,
+                                   false,
+                                   false,
+                                   omre,
+                                   w0,
+                                   wratio,
+                                   false, // screening
+                                   true,  // hole-particle
+                                   {}};
+
+  const MBPT::Sigma_params sigp_AO{MBPT::Method::Feynman,
+                                   n_min_core,
+                                   include_G,
+                                   max_l_excited,
+                                   false,
+                                   false,
+                                   omre,
+                                   w0,
+                                   wratio,
+                                   true, // screening
+                                   true, // hole-particle
+                                   {}};
+
   const MBPT::Sigma_params sigp_0{MBPT::Method::Feynman,
                                   n_min_core,
                                   include_G,
@@ -50,64 +78,108 @@ void screeningFactors(const IO::InputBlock &input, const Wavefunction &wf) {
                                   omre,
                                   w0,
                                   wratio,
-                                  false,
-                                  false,
+                                  false, // screening
+                                  false, // hole-particle
                                   {}};
 
-  const MBPT::rgrid_params gridp{1.0e-4, 30.0, stride};
+  const MBPT::rgrid_params gridp{1.0e-3, 30.0, stride};
 
   const MBPT::FeynmanSigma Sigma0(wf.getHF(), wf.basis, sigp_0, gridp, "");
-  const MBPT::FeynmanSigma SigmaX(wf.getHF(), wf.basis, sigp_X, gridp, "");
+  const MBPT::FeynmanSigma SigmaScr(wf.getHF(), wf.basis, sigp_scr, gridp, "");
+  const MBPT::FeynmanSigma Sigmahp(wf.getHF(), wf.basis, sigp_hp, gridp, "");
+  const MBPT::FeynmanSigma SigmaAO(wf.getHF(), wf.basis, sigp_AO, gridp, "");
 
   std::cout << "\n";
 
   const auto unit = PhysConst::Hartree_invcm;
 
-  std::vector<std::vector<double>> fk;
-  std::vector<int> kappa;
+  double weight = 0.0;
+  std::vector<double> fk_avg;
+  std::vector<double> fk2_avg;
+  std::vector<double> eta_avg;
+  // std::vector<int> kappa;
 
-  std::cout << "k     de(0)     de(X)     fk\n";
+  std::cout << "k   de(0)   de(X)  de(hp)  de(AO) |     fk   fkhp   eta\n";
   for (const auto &Fv : wf.valence) {
+
+    std::vector<double> fk;
+    std::vector<double> fk_hp;
+    std::vector<double> eta_hp;
+
+    // XXX Two issues:
+    // 1) For fk including hp, would be better to include holeparticle into
+    // Coulomb only, but not into loop, then divide by 2nd order.
+    // 2) For eta (hole particle correction): would be better to do for each
+    // core state, not each k!
+    // Both of these require changing FeynmanSigma class...
+
     std::cout << Fv.symbol() << "\n";
-    auto &fk_v = fk.emplace_back();
-    kappa.push_back(Fv.k);
-    for (int k = 0; k < 9; ++k) {
+    // auto &fk_v = fk.emplace_back();
+    // kappa.push_back(Fv.k);
+    for (int k = 0; k < 8; ++k) {
       auto Sd0 = Sigma0.FeynmanDirect(Fv.k, Fv.en(), k);
-      auto SdX = SigmaX.FeynmanDirect(Fv.k, Fv.en(), k);
+      auto SdX = SigmaScr.FeynmanDirect(Fv.k, Fv.en(), k);
+      auto Sdhp = Sigmahp.FeynmanDirect(Fv.k, Fv.en(), k);
+      auto SdAO = SigmaAO.FeynmanDirect(Fv.k, Fv.en(), k);
 
       const auto de0 = Fv * Sigma0.act_G_Fv(Sd0, Fv);
-      const auto deX = Fv * SigmaX.act_G_Fv(SdX, Fv);
+      const auto deX = Fv * SigmaScr.act_G_Fv(SdX, Fv);
+      const auto dehp = Fv * Sigmahp.act_G_Fv(Sdhp, Fv);
+      const auto deAO = Fv * SigmaAO.act_G_Fv(SdAO, Fv);
+
       if (std::abs(de0 * unit) < 0.01)
         break;
-      auto fkk = deX / de0;
 
-      fk_v.push_back(fkk);
+      auto t_fk = deX / de0;
+      auto t_fk_hp = deAO / dehp;
+      auto t_eta_hp = dehp / de0; // nb: do each core state, not each k!
 
-      printf("%i %9.3f %9.3f %6.3f\n", k, de0 * unit, deX * unit, fkk);
+      fk.push_back(t_fk);
+      fk_hp.push_back(t_fk_hp);
+      eta_hp.push_back(t_eta_hp);
+
+      printf("%i %7.1f %7.1f %7.1f %7.1f | %6.3f %6.3f %6.3f\n", k, de0 * unit,
+             deX * unit, dehp * unit, deAO * unit, t_fk, t_fk_hp, t_eta_hp);
     }
-  }
 
-  std::vector<double> fk_av(10);
-  std::vector<int> fk_w(10);
-  for (auto i = 0ul; i < fk.size(); ++i) {
-    std::cout << "kappa = " << kappa[i] << "\n";
-    std::cout << "fk= ";
-    for (auto j = 0ul; j < fk[i].size(); ++j) {
-      printf("%.3f, ", fk[i][j]);
-      fk_av[j] += fk[i][j];
-      fk_w[j] += 1;
-    }
+    std::cout << Fv.symbol() << "\n";
+
+    const auto printer = [](auto x) { printf("%.3f, ", x); };
+    std::cout << "fk = ";
+    std::for_each(fk.begin(), fk.end(), printer);
+    std::cout << "// no hp\n";
+    std::cout << "fk = ";
+    std::for_each(fk_hp.begin(), fk_hp.end(), printer);
+    std::cout << "// with hp\n";
+    std::cout << "eta= ";
+    std::for_each(eta_hp.begin(), eta_hp.end(), printer);
+    std::cout << "// hp only (no scr.)\n";
     std::cout << "\n";
+
+    using namespace qip::overloads;
+    fk_avg += Fv.en() * fk;
+    fk2_avg += Fv.en() * fk_hp;
+    eta_avg += Fv.en() * eta_hp;
+    weight += Fv.en();
   }
 
-  std::cout << "\nAverage:\n";
+  std::cout << "\nWeighted average:\n";
+
+  using namespace qip::overloads;
+  fk_avg /= weight;
+  fk2_avg /= weight;
+  eta_avg /= weight;
+
+  const auto printer = [](auto x) { printf("%.3f, ", x); };
   std::cout << "fk = ";
-  for (auto k = 0ul; k < fk_av.size(); ++k) {
-    if (fk_w[k] == 0)
-      break;
-    printf("%.3f, ", fk_av[k] / fk_w[k]);
-  }
-  std::cout << "\n";
+  std::for_each(fk_avg.begin(), fk_avg.end(), printer);
+  std::cout << " // no hp\n";
+  std::cout << "fk = ";
+  std::for_each(fk2_avg.begin(), fk2_avg.end(), printer);
+  std::cout << "// with hp\n";
+  std::cout << "eta= ";
+  std::for_each(eta_avg.begin(), eta_avg.end(), printer);
+  std::cout << "// hp only (no scr.)\n";
 }
 
 } // namespace Module
