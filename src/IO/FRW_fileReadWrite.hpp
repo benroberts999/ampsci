f03c38379a94ef79f38fa1f32ebfcdd314f118b3
BM Roberts <b.roberts@uq.edu.au>
2025-02-06 18:12:27 +1000

    Minor update to FRW

--- src/IO/FRW_fileReadWrite.hpp
+++ src/IO/FRW_fileReadWrite.hpp
@@ -10,109 +10,19 @@
 #include <vector>
 /*!
 Functions to help read-write data to files, including binary files.
+Very old and poorly written..but works.
 */
 namespace IO::FRW {
 
 //==============================================================================
-// Uses compile-time recursion to get access to elements of tuple.
-// Specifically, string-streams data from a string vector into tuple.
-// Works with a tuple of references, i.e., std::forward_as_tuple
-// Idea from:
-// https://stackoverflow.com/questions/1198260/iterate-over-tuple/23142715
-template <std::size_t I = 0, typename... Tp>
-inline typename std::enable_if<I == sizeof...(Tp), void>::type
-stringstreamVectorIntoTuple(const std::vector<std::string> &,
-                            std::tuple<Tp...> &) {}
-
-template <std::size_t I = 0, typename... Tp>
-    inline typename std::enable_if <
-    I<sizeof...(Tp), void>::type
-    stringstreamVectorIntoTuple(const std::vector<std::string> &lst,
-                                std::tuple<Tp...> &t) {
-  if (I > lst.size())
-    std::cerr << "\nFAIL 34 in FRW: list shorter than tuple\n";
-  std::stringstream(lst[I]) >> std::get<I>(t);
-  stringstreamVectorIntoTuple<I + 1, Tp...>(lst, t);
-}
-
-//==============================================================================
-inline std::vector<std::string> readInputFile_byEntry(const std::string &fname)
-// Reads each item (space separated) from a file into a string vector
-// Any text after a '#' or '!' are treated as comments and ignored
-// (can come anywhere in the line)
-{
-  std::vector<std::string> entry_list;
-  std::ifstream file(fname);
-  std::string line;
-  while (getline(file, line) && (file.is_open())) { // redundant?
-    std::stringstream ss(line);
-    std::string entry;
-    while (ss >> entry) {
-      if (entry.at(0) == '!' || entry.at(0) == '#')
-        break;
-      if (entry.size() >= 2 && entry.at(0) == '/' && entry.at(1) == '/')
-        break;
-      entry_list.push_back(entry);
-    }
-  }
-  return entry_list;
-}
-
-//==============================================================================
-inline std::vector<std::string> readInputFile_byLine(const std::string &fname)
-// Reads each line from a file into a string vector
-// Lines beginning with '!' or '#' are comments
-{
-  std::vector<std::string> entry_list;
-  std::ifstream file(fname);
-  std::string line;
-  while (getline(file, line) && (file.is_open())) { // redundant?
-    if (line == "")
-      continue;
-    if (line.at(0) == '!' || line.at(0) == '#')
-      continue;
-    if (line.size() >= 2 && line.at(0) == '/' && line.at(1) == '/')
-      continue;
-    entry_list.push_back(line);
-  }
-  return entry_list;
-}
-
-//==============================================================================
-inline std::vector<std::pair<double, double>>
-readFile_xy_VoP(const std::string &fname)
-// Reads each line from a file into a vector of {x,y} points
-// Lines beginning with '!' or '#' are comments
-// Could generalise this to any number of points?
-{
-  std::vector<std::pair<double, double>> out_list;
-  std::ifstream file(fname);
-  std::string line = "";
-  while (getline(file, line) && (file.is_open())) { // redundant?
-    if (line == "")
-      continue;
-    if (line.at(0) == '!' || line.at(0) == '#')
-      continue;
-    if (line.size() >= 2 && line.at(0) == '/' && line.at(1) == '/')
-      continue;
-    std::stringstream ss(line);
-    double x, y;
-    ss >> x >> y;
-    out_list.emplace_back(x, y);
-  }
-  return out_list;
-}
-//------------------------------------------------------------------------------
+//! Reads a text file line-by-line into a pair of vectors.
+//! Ignores comments (!, #, //)
 inline std::pair<std::vector<double>, std::vector<double>>
-readFile_xy_PoV(const std::string &fname)
-// Reads each line from a file;
-// Returns a pair of a vectors {{x},{y}}
-// Lines beginning with '!' or '#' are comments
-{
+readFile_xy_PoV(const std::string &fname) {
   std::pair<std::vector<double>, std::vector<double>> out_list;
   std::ifstream file(fname);
   std::string line = "";
-  while (getline(file, line) && (file.is_open())) { // redundant?
+  while (getline(file, line) && (file.is_open())) {
     if (line == "")
       continue;
     if (line.at(0) == '!' || line.at(0) == '#')
@@ -128,37 +38,8 @@ readFile_xy_PoV(const std::string &fname)
   return out_list;
 }
 
-//------------------------------------------------------------------------------
-inline void writeFile_xy(const std::vector<double> &x,
-                         const std::vector<double> &y,
-                         const std::string &fname) {
-  //
-  if (x.size() != y.size()) {
-    std::cout << "Warning 139 in FRW: trying to write {x,y} vectors of "
-                 "different lengths!\n";
-  }
-  std::ofstream file(fname);
-  auto max = std::min(x.size(), y.size());
-  for (auto i = 0ul; i < max; ++i) {
-    file << x[i] << " " << y[i] << "\n";
-  }
-  //
-}
-
 //==============================================================================
-//! reads entire text file into a string
-inline std::string readInputFile(const std::string &fname) {
-  std::ifstream f(fname); // taking file as inputstream
-  std::string str;
-  if (f) {
-    std::ostringstream ss;
-    ss << f.rdbuf(); // reading data
-    str = ss.str();
-  }
-  return str;
-}
-
-//==============================================================================
+//! Removes block comments from a string
 inline void removeBlockComments(std::string &input) {
   for (auto posi = input.find("/*"); posi != std::string::npos;
        posi = input.find("/*")) {
@@ -171,9 +52,10 @@ inline void removeBlockComments(std::string &input) {
   }
 }
 //==============================================================================
-inline std::string removeCommentsAndSpaces(const std::string &input)
-// Note: also squashes lines, except for semi-colons
-{
+//! Removes comments, spaces/tabs, quotes and newlines from a string
+//! (comments: !, #, //, and block /**/ comments)
+inline std::string removeCommentsAndSpaces(const std::string &input) {
+  // Note: also squashes lines, except for semi-colons
   std::string lines = "";
   {
     std::string line;
@@ -210,6 +92,8 @@ inline std::string removeCommentsAndSpaces(const std::string &input)
 }
 
 //==============================================================================
+//! Splits an input string into a vector of pairs of strings; split by '{}'
+//! For example, ABC{DE} -> [ABC,DE]
 inline std::vector<std::pair<std::string, std::string>>
 splitInput_byBraces(const std::string &input) {
 
@@ -257,6 +141,7 @@ splitInput_byBraces(const std::string &input) {
 }
 
 //==============================================================================
+//! Splits a string into a vector of strings, by semicolon... with exceptions..
 inline std::vector<std::string> splitInput_bySemiColon(const std::string &input)
 // ...
 {
@@ -267,16 +152,10 @@ inline std::vector<std::string> splitInput_bySemiColon(const std::string &input)
   // BUT, treat anything inside [] brackets as single
 
   // after removing comments, break up by ';'
-  // std::stringstream stream2(lines);
-  // std::string entry;
-  // while (std::getline(stream2, entry, ';')) {
-  //   entry_list.push_back(entry);
-  // }
 
   // dumb hack to ingore first braket '['..
   std::size_t start = (input.size() > 0 && input[0] == '{') ? 1 : 0;
 
-  // std::size_t start = 0;
   while (true) {
     // Check if we find a ';' or an open braket '[' first:
     const auto bkt = input.find('{', start);
@@ -293,23 +172,6 @@ inline std::vector<std::string> splitInput_bySemiColon(const std::string &input)
   }
 
   return entry_list;
-}
-
-//==============================================================================
-template <typename... Tp>
-void setInputParameters(const std::string &infile, std::tuple<Tp...> &tp) {
-  auto input = readInputFile_byEntry(infile);
-  if (sizeof...(Tp) > input.size()) {
-    // Note: for now, I allow a longer-than-needed input list.
-    // This allows us to not have to comment out all the crap below
-    // the input file..
-    std::cerr << "\nFail 71 in FRW: Wrong number of input parameters? "
-              << "Reading from file: " << infile << ". Expected "
-              << sizeof...(Tp) << " arguments"
-              << ", but got " << input.size() << ".\n";
-    return;
-  }
-  stringstreamVectorIntoTuple(input, tp);
 }
 
 //==============================================================================
