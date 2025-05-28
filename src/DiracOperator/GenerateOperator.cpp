8dfaf647b5aa294af1ea1e1c57e4768d9a2b5220
BM Roberts <b.roberts@uq.edu.au>
2025-05-28 18:00:57 +1000

    Adds electric multipole operator

--- src/DiracOperator/GenerateOperator.cpp
+++ src/DiracOperator/GenerateOperator.cpp
@@ -1,30 +1,6 @@
 #include "GenerateOperator.hpp"
-#include "Operators/Ek.hpp"
-#include "Operators/FieldShift.hpp"
-#include "Operators/M1.hpp"
-#include "Operators/PNC.hpp"
-#include "Operators/QED.hpp"
-#include "Operators/RadialF.hpp"
-#include "Operators/hfs.hpp"
-#include "Operators/jls.hpp"
-#include "Operators/p.hpp"
 
 namespace DiracOperator {
-
-//! List of avilable operators, and their generator functions
-static const std::vector<std::pair<
-    std::string, std::unique_ptr<DiracOperator::TensorOperator> (*)(
-                     const IO::InputBlock &input, const Wavefunction &wf)>>
-    operator_list{
-        {"E1", &generate_E1},     {"E1v", &generate_E1v},
-        {"E2", &generate_E2},     {"Ek", &generate_Ek},
-        {"M1", &generate_M1},     {"M1nr", &generate_M1nr},
-        {"hfs", &generate_hfs},   {"fieldshift", &generate_fieldshift},
-        {"r", &generate_r},       {"sigma_r", &generate_sigma_r},
-        {"pnc", &generate_pnc},   {"Vrad", &generate_Vrad},
-        {"MLVP", &generate_MLVP}, {"dr", &generate_dr},
-        {"p", &generate_p},       {"l", &generate_l},
-        {"s", &generate_s}};
 
 //--------------------------------------------------------------------
 std::unique_ptr<DiracOperator::TensorOperator>
--- src/DiracOperator/GenerateOperator.hpp
+++ src/DiracOperator/GenerateOperator.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "IO/InputBlock.hpp"
+#include "Operators.hpp"
 #include "TensorOperator.hpp"
 #include "Wavefunction/Wavefunction.hpp"
 #include <memory>
@@ -7,6 +8,29 @@
 #include <vector>
 
 namespace DiracOperator {
+
+//! List of avilable operators, and their generator functions
+static const std::vector<std::pair<
+    std::string, std::unique_ptr<DiracOperator::TensorOperator> (*)(
+                     const IO::InputBlock &input, const Wavefunction &wf)>>
+    operator_list{{"E1", &generate_E1},
+                  {"E1v", &generate_E1v},
+                  {"E2", &generate_E2},
+                  {"Ek", &generate_Ek},
+                  {"Ek_omega", &generate_Ek_omega},
+                  {"M1", &generate_M1},
+                  {"M1nr", &generate_M1nr},
+                  {"hfs", &generate_hfs},
+                  {"fieldshift", &generate_fieldshift},
+                  {"r", &generate_r},
+                  {"sigma_r", &generate_sigma_r},
+                  {"pnc", &generate_pnc},
+                  {"Vrad", &generate_Vrad},
+                  {"MLVP", &generate_MLVP},
+                  {"dr", &generate_dr},
+                  {"p", &generate_p},
+                  {"l", &generate_l},
+                  {"s", &generate_s}};
 
 //------------------------------------------------------------------------------
 
