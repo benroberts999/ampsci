2f3571bee13c83868fd9964de524f20d81c2569b
BM Roberts <b.roberts@uq.edu.au>
2026-02-12 11:56:05 +1000

    Moves several Multipole functions from .hpp to .cpp

--- src/DiracOperator/Operators/EM_multipole.cpp
+++ src/DiracOperator/Operators/EM_multipole.cpp
@@ -3,6 +3,8 @@
 namespace DiracOperator {
 
 //==============================================================================
+// VEk_Len::radial_rhs
+//==============================================================================
 
 DiracSpinor VEk_Len::radial_rhs(const int kappa_a,
                                 const DiracSpinor &Fb) const {
@@ -22,6 +24,246 @@ DiracSpinor VEk_Len::radial_rhs(const int kappa_a,
   Rab_rhs(+1, *p_jK, &dF, Fb, cx);
   Pab_rhs(+1, *p_jKp1, &dF, Fb, -c1 * cx);
   Pab_rhs(-1, *p_jKp1, &dF, Fb, -cx);
+  return dF;
+}
+
+//==============================================================================
+// VEk::radial_rhs
+//==============================================================================
+
+DiracSpinor VEk::radial_rhs(const int kappa_a, const DiracSpinor &Fb) const {
+
+  DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
+  dF.min_pt() = Fb.min_pt();
+  dF.max_pt() = Fb.max_pt();
+
+  if (isZero(kappa_a, Fb.kappa()) || m_K == 0) {
+    dF.min_pt() = 0;
+    dF.max_pt() = 0;
+    return dF;
+  }
+
+  const auto K = double(m_K);
+  const auto dk = double(kappa_a - Fb.kappa());
+  assert(m_K != 0); // should already be discounted!
+  const auto cx = std::sqrt((K + 1.0) / K);
+  if (dk != 0.0) {
+    Pab_rhs(+1, *p_jK_on_qr, &dF, Fb, cx * dk);
+    Pab_rhs(+1, *p_jKp1, &dF, Fb, -cx * dk / (K + 1.0));
+  }
+  Pab_rhs(-1, *p_jK_on_qr, &dF, Fb, -cx * K);
+
+  return dF;
+}
+
+//==============================================================================
+// VLk::radial_rhs
+//==============================================================================
+
+DiracSpinor VLk::radial_rhs(const int kappa_a, const DiracSpinor &Fb) const {
+
+  DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
+  dF.min_pt() = Fb.min_pt();
+  dF.max_pt() = Fb.max_pt();
+
+  if (isZero(kappa_a, Fb.kappa())) {
+    dF.min_pt() = 0;
+    dF.max_pt() = 0;
+    return dF;
+  }
+
+  const auto K = double(m_K);
+  const auto dk = double(kappa_a - Fb.kappa());
+
+  if (dk != 0.0)
+    Pab_rhs(+1, *p_jK_on_qr, &dF, Fb, -dk);
+  if (K != 0.0)
+    Pab_rhs(-1, *p_jK_on_qr, &dF, Fb, K);
+  Pab_rhs(-1, *p_jKp1, &dF, Fb, -1.0);
+  return dF;
+}
+
+//==============================================================================
+// VMk::radial_rhs
+//==============================================================================
+
+DiracSpinor VMk::radial_rhs(const int kappa_a, const DiracSpinor &Fb) const {
+
+  DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
+  dF.min_pt() = Fb.min_pt();
+  dF.max_pt() = Fb.max_pt();
+
+  if (isZero(kappa_a, Fb.kappa()) || (kappa_a == -Fb.kappa()) || m_K == 0) {
+    dF.min_pt() = 0;
+    dF.max_pt() = 0;
+    return dF;
+  }
+
+  const auto K = double(m_K);
+  const auto sk = double(kappa_a + Fb.kappa());
+  assert(m_K != 0); // should already be discounted!
+  const auto ck = sk / std::sqrt(K * (K + 1.0));
+
+  Pab_rhs(+1, *p_jK, &dF, Fb, -ck);
+  return dF;
+}
+
+//==============================================================================
+// Phik::radial_rhs
+//==============================================================================
+
+DiracSpinor Phik::radial_rhs(const int kappa_a, const DiracSpinor &Fb) const {
+
+  DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
+  dF.min_pt() = Fb.min_pt();
+  dF.max_pt() = Fb.max_pt();
+
+  if (isZero(kappa_a, Fb.kappa())) {
+    dF.min_pt() = 0;
+    dF.max_pt() = 0;
+    return dF;
+  }
+
+  Rab_rhs(+1, *p_jK, &dF, Fb);
+  return dF;
+}
+
+//==============================================================================
+// Sk::radial_rhs
+//==============================================================================
+
+DiracSpinor Sk::radial_rhs(const int kappa_a, const DiracSpinor &Fb) const {
+
+  DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
+  dF.min_pt() = Fb.min_pt();
+  dF.max_pt() = Fb.max_pt();
+
+  if (isZero(kappa_a, Fb.kappa())) {
+    dF.min_pt() = 0;
+    dF.max_pt() = 0;
+    return dF;
+  }
+
+  Rab_rhs(-1, *p_jK, &dF, Fb);
+  return dF;
+}
+
+//==============================================================================
+// E5k_w::radial_rhs
+//==============================================================================
+
+DiracSpinor E5k_w::radial_rhs(const int kappa_a, const DiracSpinor &Fb) const {
+
+  DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
+  dF.min_pt() = Fb.min_pt();
+  dF.max_pt() = Fb.max_pt();
+
+  if (isZero(kappa_a, Fb.kappa()) || m_K == 0) {
+    dF.min_pt() = 0;
+    dF.max_pt() = 0;
+    return dF;
+  }
+
+  const auto K = double(m_K);
+  const auto dk = double(kappa_a + Fb.kappa());
+  assert(m_K != 0); // should already be discounted!
+  const auto cx = std::sqrt((K + 1.0) / K);
+
+  Rab_rhs(-1, jK_on_qr, &dF, Fb, cx * dk);
+  Rab_rhs(-1, jKp1, &dF, Fb, -cx * dk / (K + 1.0));
+  Rab_rhs(+1, jK_on_qr, &dF, Fb, -cx * K);
+
+  return dF;
+}
+
+//==============================================================================
+// L5k_w::radial_rhs
+//==============================================================================
+
+DiracSpinor L5k_w::radial_rhs(const int kappa_a, const DiracSpinor &Fb) const {
+
+  DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
+  dF.min_pt() = Fb.min_pt();
+  dF.max_pt() = Fb.max_pt();
+
+  if (isZero(kappa_a, Fb.kappa())) {
+    dF.min_pt() = 0;
+    dF.max_pt() = 0;
+    return dF;
+  }
+
+  const auto K = double(m_K);
+  const auto dk = double(kappa_a + Fb.kappa());
+
+  Rab_rhs(-1, jK_on_qr, &dF, Fb, -dk);
+  Rab_rhs(+1, jK_on_qr, &dF, Fb, K);
+  Rab_rhs(+1, jKp1, &dF, Fb, -1.0);
+  return dF;
+}
+
+//==============================================================================
+// M5k_w::radial_rhs
+//==============================================================================
+
+DiracSpinor M5k_w::radial_rhs(const int kappa_a, const DiracSpinor &Fb) const {
+
+  DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
+  dF.min_pt() = Fb.min_pt();
+  dF.max_pt() = Fb.max_pt();
+
+  if (isZero(kappa_a, Fb.kappa()) || (kappa_a == -Fb.kappa()) || m_K == 0) {
+    dF.min_pt() = 0;
+    dF.max_pt() = 0;
+    return dF;
+  }
+
+  const auto K = double(m_K);
+  const auto sk = double(kappa_a - Fb.kappa());
+  assert(m_K != 0); // should already be discounted!
+  const auto ck = sk / std::sqrt(K * (K + 1.0));
+
+  Rab_rhs(-1, jK, &dF, Fb, -ck);
+  return dF;
+}
+
+//==============================================================================
+// Phi5k_w::radial_rhs
+//==============================================================================
+
+DiracSpinor Phi5k_w::radial_rhs(const int kappa_a,
+                                const DiracSpinor &Fb) const {
+
+  DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
+  dF.min_pt() = Fb.min_pt();
+  dF.max_pt() = Fb.max_pt();
+
+  if (isZero(kappa_a, Fb.kappa())) {
+    dF.min_pt() = 0;
+    dF.max_pt() = 0;
+    return dF;
+  }
+
+  Pab_rhs(-1, jk, &dF, Fb);
+  return dF;
+}
+
+//==============================================================================
+// S5k_w::radial_rhs
+//==============================================================================
+
+DiracSpinor S5k_w::radial_rhs(const int kappa_a, const DiracSpinor &Fb) const {
+
+  DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
+  dF.min_pt() = Fb.min_pt();
+  dF.max_pt() = Fb.max_pt();
+
+  if (isZero(kappa_a, Fb.kappa())) {
+    dF.min_pt() = 0;
+    dF.max_pt() = 0;
+    return dF;
+  }
+
+  Pab_rhs(+1, jk, &dF, Fb);
   return dF;
 }
 
--- src/DiracOperator/Operators/EM_multipole.hpp
+++ src/DiracOperator/Operators/EM_multipole.hpp
@@ -150,30 +150,7 @@ public:
 
   //--------------
   DiracSpinor radial_rhs(const int kappa_a,
-                         const DiracSpinor &Fb) const override final {
-
-    DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
-    dF.min_pt() = Fb.min_pt();
-    dF.max_pt() = Fb.max_pt();
-
-    if (isZero(kappa_a, Fb.kappa()) || m_K == 0) {
-      dF.min_pt() = 0;
-      dF.max_pt() = 0;
-      return dF;
-    }
-
-    const auto K = double(m_K);
-    const auto dk = double(kappa_a - Fb.kappa());
-    assert(m_K != 0); // should already be discounted!
-    const auto cx = std::sqrt((K + 1.0) / K);
-    if (dk != 0.0) {
-      Pab_rhs(+1, *p_jK_on_qr, &dF, Fb, cx * dk);
-      Pab_rhs(+1, *p_jKp1, &dF, Fb, -cx * dk / (K + 1.0));
-    }
-    Pab_rhs(-1, *p_jK_on_qr, &dF, Fb, -cx * K);
-
-    return dF;
-  }
+                         const DiracSpinor &Fb) const override final;
 
   //--------------
   double radialIntegral(const DiracSpinor &Fa,
@@ -263,28 +240,7 @@ public:
 
   //--------------
   DiracSpinor radial_rhs(const int kappa_a,
-                         const DiracSpinor &Fb) const override final {
-
-    DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
-    dF.min_pt() = Fb.min_pt();
-    dF.max_pt() = Fb.max_pt();
-
-    if (isZero(kappa_a, Fb.kappa())) {
-      dF.min_pt() = 0;
-      dF.max_pt() = 0;
-      return dF;
-    }
-
-    const auto K = double(m_K);
-    const auto dk = double(kappa_a - Fb.kappa());
-
-    if (dk != 0.0)
-      Pab_rhs(+1, *p_jK_on_qr, &dF, Fb, -dk);
-    if (K != 0.0)
-      Pab_rhs(-1, *p_jK_on_qr, &dF, Fb, K);
-    Pab_rhs(-1, *p_jKp1, &dF, Fb, -1.0);
-    return dF;
-  }
+                         const DiracSpinor &Fb) const override final;
 
   //--------------
   double radialIntegral(const DiracSpinor &Fa,
@@ -369,26 +325,7 @@ public:
 
   //--------------
   DiracSpinor radial_rhs(const int kappa_a,
-                         const DiracSpinor &Fb) const override final {
-
-    DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
-    dF.min_pt() = Fb.min_pt();
-    dF.max_pt() = Fb.max_pt();
-
-    if (isZero(kappa_a, Fb.kappa()) || (kappa_a == -Fb.kappa()) || m_K == 0) {
-      dF.min_pt() = 0;
-      dF.max_pt() = 0;
-      return dF;
-    }
-
-    const auto K = double(m_K);
-    const auto sk = double(kappa_a + Fb.kappa());
-    assert(m_K != 0); // should already be discounted!
-    const auto ck = sk / std::sqrt(K * (K + 1.0));
-
-    Pab_rhs(+1, *p_jK, &dF, Fb, -ck);
-    return dF;
-  }
+                         const DiracSpinor &Fb) const override final;
 
   //--------------
   double radialIntegral(const DiracSpinor &Fa,
@@ -466,21 +403,7 @@ public:
 
   //--------------
   DiracSpinor radial_rhs(const int kappa_a,
-                         const DiracSpinor &Fb) const override final {
-
-    DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
-    dF.min_pt() = Fb.min_pt();
-    dF.max_pt() = Fb.max_pt();
-
-    if (isZero(kappa_a, Fb.kappa())) {
-      dF.min_pt() = 0;
-      dF.max_pt() = 0;
-      return dF;
-    }
-
-    Rab_rhs(+1, *p_jK, &dF, Fb);
-    return dF;
-  }
+                         const DiracSpinor &Fb) const override final;
 
   //--------------
   double radialIntegral(const DiracSpinor &Fa,
@@ -553,21 +476,7 @@ public:
 
   //--------------
   DiracSpinor radial_rhs(const int kappa_a,
-                         const DiracSpinor &Fb) const override final {
-
-    DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
-    dF.min_pt() = Fb.min_pt();
-    dF.max_pt() = Fb.max_pt();
-
-    if (isZero(kappa_a, Fb.kappa())) {
-      dF.min_pt() = 0;
-      dF.max_pt() = 0;
-      return dF;
-    }
-
-    Rab_rhs(-1, *p_jK, &dF, Fb);
-    return dF;
-  }
+                         const DiracSpinor &Fb) const override final;
 
   //--------------
   double radialIntegral(const DiracSpinor &Fa,
@@ -643,29 +552,7 @@ public:
 
   //--------------
   DiracSpinor radial_rhs(const int kappa_a,
-                         const DiracSpinor &Fb) const override final {
-
-    DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
-    dF.min_pt() = Fb.min_pt();
-    dF.max_pt() = Fb.max_pt();
-
-    if (isZero(kappa_a, Fb.kappa()) || m_K == 0) {
-      dF.min_pt() = 0;
-      dF.max_pt() = 0;
-      return dF;
-    }
-
-    const auto K = double(m_K);
-    const auto dk = double(kappa_a + Fb.kappa());
-    assert(m_K != 0); // should already be discounted!
-    const auto cx = std::sqrt((K + 1.0) / K);
-
-    Rab_rhs(-1, jK_on_qr, &dF, Fb, cx * dk);
-    Rab_rhs(-1, jKp1, &dF, Fb, -cx * dk / (K + 1.0));
-    Rab_rhs(+1, jK_on_qr, &dF, Fb, -cx * K);
-
-    return dF;
-  }
+                         const DiracSpinor &Fb) const override final;
 
   //--------------
   double radialIntegral(const DiracSpinor &Fa,
@@ -745,26 +632,7 @@ public:
 
   //--------------
   DiracSpinor radial_rhs(const int kappa_a,
-                         const DiracSpinor &Fb) const override final {
-
-    DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
-    dF.min_pt() = Fb.min_pt();
-    dF.max_pt() = Fb.max_pt();
-
-    if (isZero(kappa_a, Fb.kappa())) {
-      dF.min_pt() = 0;
-      dF.max_pt() = 0;
-      return dF;
-    }
-
-    const auto K = double(m_K);
-    const auto dk = double(kappa_a + Fb.kappa());
-
-    Rab_rhs(-1, jK_on_qr, &dF, Fb, -dk);
-    Rab_rhs(+1, jK_on_qr, &dF, Fb, K);
-    Rab_rhs(+1, jKp1, &dF, Fb, -1.0);
-    return dF;
-  }
+                         const DiracSpinor &Fb) const override final;
 
   //--------------
   double radialIntegral(const DiracSpinor &Fa,
@@ -840,26 +708,7 @@ public:
 
   //--------------
   DiracSpinor radial_rhs(const int kappa_a,
-                         const DiracSpinor &Fb) const override final {
-
-    DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
-    dF.min_pt() = Fb.min_pt();
-    dF.max_pt() = Fb.max_pt();
-
-    if (isZero(kappa_a, Fb.kappa()) || (kappa_a == -Fb.kappa()) || m_K == 0) {
-      dF.min_pt() = 0;
-      dF.max_pt() = 0;
-      return dF;
-    }
-
-    const auto K = double(m_K);
-    const auto sk = double(kappa_a - Fb.kappa());
-    assert(m_K != 0); // should already be discounted!
-    const auto ck = sk / std::sqrt(K * (K + 1.0));
-
-    Rab_rhs(-1, jK, &dF, Fb, -ck);
-    return dF;
-  }
+                         const DiracSpinor &Fb) const override final;
 
   //--------------
   double radialIntegral(const DiracSpinor &Fa,
@@ -931,21 +780,7 @@ public:
 
   //--------------
   DiracSpinor radial_rhs(const int kappa_a,
-                         const DiracSpinor &Fb) const override final {
-
-    DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
-    dF.min_pt() = Fb.min_pt();
-    dF.max_pt() = Fb.max_pt();
-
-    if (isZero(kappa_a, Fb.kappa())) {
-      dF.min_pt() = 0;
-      dF.max_pt() = 0;
-      return dF;
-    }
-
-    Pab_rhs(-1, jk, &dF, Fb);
-    return dF;
-  }
+                         const DiracSpinor &Fb) const override final;
 
   //--------------
   double radialIntegral(const DiracSpinor &Fa,
@@ -1010,21 +845,7 @@ public:
 
   //--------------
   DiracSpinor radial_rhs(const int kappa_a,
-                         const DiracSpinor &Fb) const override final {
-
-    DiracSpinor dF(0, kappa_a, Fb.grid_sptr());
-    dF.min_pt() = Fb.min_pt();
-    dF.max_pt() = Fb.max_pt();
-
-    if (isZero(kappa_a, Fb.kappa())) {
-      dF.min_pt() = 0;
-      dF.max_pt() = 0;
-      return dF;
-    }
-
-    Pab_rhs(+1, jk, &dF, Fb);
-    return dF;
-  }
+                         const DiracSpinor &Fb) const override final;
 
   //--------------
   double radialIntegral(const DiracSpinor &Fa,
