000841b29fceca49448a09f4f68b9239299150eb
BM Roberts <b.roberts@uq.edu.au>
2026-02-17 19:32:02 +1000

    Minor update to continuum orbital solver

--- src/Wavefunction/ContinuumOrbitals.cpp
+++ src/Wavefunction/ContinuumOrbitals.cpp
@@ -13,10 +13,10 @@
 #include <vector>
 //==============================================================================
 ContinuumOrbitals::ContinuumOrbitals(const Wavefunction &wf)
-  : rgrid(wf.grid_sptr()), p_hf(wf.vHF()), alpha(wf.alpha()) {}
+  : p_rgrid(wf.grid_sptr()), p_hf(wf.vHF()), m_alpha(wf.alpha()) {}
 
 ContinuumOrbitals::ContinuumOrbitals(const HF::HartreeFock *hf)
-  : rgrid(hf->grid_sptr()), p_hf(hf), alpha(hf->alpha()) {}
+  : p_rgrid(hf->grid_sptr()), p_hf(hf), m_alpha(hf->alpha()) {}
 
 //==============================================================================
 double ContinuumOrbitals::check_orthog(bool print) const {
@@ -62,22 +62,22 @@ int ContinuumOrbitals::solveContinuumHF(double ec, int min_l, int max_l,
   }
 
   // We may wish to do this to test things, but not for final calculations:
-  if (force_rescale && subtract_self) {
-    fmt2::styled_print(fg(fmt::color::orange), "\nWARNING 65: ");
-    fmt::print("Should not subtract self interaction _and_ "
-               "rescale V(r): do one or the other\n");
-  }
+  // if (force_rescale && subtract_self) {
+  //   fmt2::styled_print(fg(fmt::color::orange), "\nWARNING 65: ");
+  //   fmt::print("Should not subtract self interaction _and_ "
+  //              "rescale V(r): do one or the other\n");
+  // }
 
   //Optionally, re-scale large-r part of local potential,
   // so goes like -1/r large r
   // Note: doesn't inclue exchange..
   // This also kills orthogonality for HF...
   if (force_rescale && self_consistant) {
-    const auto Zion = std::max(1.0, -vc.back() * rgrid->r().back());
+    const auto Zion = std::max(1.0, -vc.back() * p_rgrid->r().back());
     // nb: this agrees best with Dzuba, but bad for orthog
-    for (std::size_t i = 0; i < rgrid->num_points(); ++i) {
-      if (vc[i] > -Zion / rgrid->r(i)) {
-        vc[i] = -Zion / rgrid->r(i);
+    for (std::size_t i = 0; i < p_rgrid->num_points(); ++i) {
+      if (vc[i] > -Zion / p_rgrid->r(i)) {
+        vc[i] = -Zion / p_rgrid->r(i);
       }
     }
   }
@@ -91,16 +91,15 @@ int ContinuumOrbitals::solveContinuumHF(double ec, int min_l, int max_l,
     if (l > max_l)
       break;
 
-    auto &Fc = orbitals.emplace_back(0, kappa, rgrid);
+    auto &Fc = orbitals.emplace_back(0, kappa, p_rgrid);
     Fc.en() = ec;
     // solve initial, without exchange term
-    DiracODE::solveContinuum(Fc, ec, vc, alpha);
+    DiracODE::solveContinuum(Fc, ec, vc, m_alpha);
     // Then, include exchange correction:
     if (p_hf != nullptr && !p_hf->excludeExchangeQ()) {
       IncludeExchange(Fc, Fi, force_orthog_Fi, vc);
     }
-
-  } // kappa
+  }
 
   // Orthogonalise against entire core:
   if (orthog_core) {
@@ -109,8 +108,13 @@ int ContinuumOrbitals::solveContinuumHF(double ec, int min_l, int max_l,
         if (Fa.kappa() == Fc.kappa())
           Fc -= (Fc * Fa) * Fa;
       }
-      // orthod wrt rest of core can slightly ruin "main" orthog condition
-      if (force_orthog_Fi && Fi != nullptr && Fi->kappa() == Fc.kappa()) {
+    }
+  }
+
+  // Force orthogonality to specific state
+  if (force_orthog_Fi && Fi != nullptr) {
+    for (auto &Fc : orbitals) {
+      if (Fi->kappa() == Fc.kappa()) {
         Fc -= (*Fi * Fc) * *Fi;
       }
     }
@@ -135,39 +139,43 @@ void ContinuumOrbitals::IncludeExchange(DiracSpinor &Fc, const DiracSpinor *Fi,
     const auto Fc0 = Fc;
     if (p_hf->method() == HF::Method::HartreeFock) {
       auto VxFc = HF::vexFa(Fc, p_hf->core()) - vx0 * Fc;
-      DiracODE::solveContinuum(Fc, Fc.en(), vl, alpha, &VxFc, &Fc0);
-    } else { // HF::Method::ApproxHF)
-      DiracODE::solveContinuum(Fc, Fc.en(), vl, alpha);
+      DiracODE::solveContinuum(Fc, Fc.en(), vl, m_alpha, &VxFc, &Fc0);
+    } else {
+      DiracODE::solveContinuum(Fc, Fc.en(), vl, m_alpha);
     }
-    // Force orthogonality to Fi (ionised state) (at each HF step)
-    if (force_orthog_Fi && Fi && Fi->kappa() == Fc.kappa()) {
-      Fc -= (*Fi * Fc) * *Fi;
-    }
+
     // check convergance:
     const auto eps = ((Fc0 - Fc) * (Fc0 - Fc)) / (Fc * Fc);
     if (eps < conv_target || it == max_its) {
       break;
     }
+
     // Damp the orbital
-    Fc = 0.5 * (Fc + Fc0);
-  } // it
+    // Fc = 0.5 * (Fc + Fc0);
+    Fc += Fc0;
+    Fc *= 0.5;
+
+    // Force orthogonality to Fi (ionised state) (at each HF step)
+    if (force_orthog_Fi && Fi && Fi->kappa() == Fc.kappa()) {
+      Fc -= (*Fi * Fc) * *Fi;
+    }
+  }
 }
 
 //******************************************************************************
 int ContinuumOrbitals::solveContinuumZeff(double ec, int min_l, int max_l,
                                           double Z_eff, const DiracSpinor *Fi,
-                                          bool force_orthog)
-// Solves Dirac equation for H-like potential (Zeff model)
-// Same Zeff as used by DarkARC (eqn B35 of arxiv:1912.08204):
-// Zeff = sqrt{I_{njl} eV / 13.6 eV} * n
-// au: Zeff = sqrt{2 * I_{njl}} * n
-{
+                                          bool force_orthog) {
+  // Solves Dirac equation for H-like potential (Zeff model)
+  // Same Zeff as used by DarkARC (eqn B35 of arxiv:1912.08204):
+  // Zeff = sqrt{I_{njl} eV / 13.6 eV} * n
+  // au: Zeff = sqrt{2 * I_{njl}} * n
 
   // Also orthogonalise against entire core: (make no difference)
   const bool orthog_core = force_orthog;
 
   // Zeff potential (pointlike nucleus, spherical with Rn=0):
-  const auto vc = Nuclear::sphericalNuclearPotential(Z_eff, 0.0, rgrid->r());
+  const auto vc = Nuclear::sphericalNuclearPotential(Z_eff, 0.0, p_rgrid->r());
 
   // loop through each kappa state
   for (int k_i = 0; true; ++k_i) {
@@ -178,10 +186,10 @@ int ContinuumOrbitals::solveContinuumZeff(double ec, int min_l, int max_l,
     if (l > max_l)
       break;
 
-    auto &Fc = orbitals.emplace_back(0, kappa, rgrid);
+    auto &Fc = orbitals.emplace_back(0, kappa, p_rgrid);
     Fc.en() = ec;
     // solve initial, without exchange term
-    DiracODE::solveContinuum(Fc, ec, vc, alpha);
+    DiracODE::solveContinuum(Fc, ec, vc, m_alpha);
 
   } // kappa
 
