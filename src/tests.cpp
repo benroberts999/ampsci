cefee5048d8117ac1ef74bc4aa04ac969295bc12
BM Roberts <b.roberts@uq.edu.au>
2026-03-10 08:38:06 +1000

    Adds unit test headers in blue

--- src/tests.cpp
+++ src/tests.cpp
@@ -2,8 +2,29 @@
 #define CATCH_CONFIG_RUNNER
 #include "IO/ChronoTimer.hpp"
 #include "catch2/catch.hpp"
+#include "fmt/color.hpp"
 #include "version/version.hpp"
 #include <iostream>
+
+class MyTestPrinter : public Catch::TestEventListenerBase {
+public:
+  using TestEventListenerBase::TestEventListenerBase;
+
+  void testCaseStarting(Catch::TestCaseInfo const &testInfo) override {
+
+    fmt2::styled_print(fg(fmt::color::blue), "\n=============================="
+                                             "============================="
+                                             "====================\n");
+    fmt2::styled_print(fg(fmt::color::blue), testInfo.name + '\n');
+    fmt2::styled_print(fg(fmt::color::blue), "Tags: ");
+    std::cout << testInfo.tagsAsString() << '\n';
+    fmt2::styled_print(fg(fmt::color::blue), "------------------------------"
+                                             "-----------------------------"
+                                             "--------------------\n");
+  }
+};
+
+CATCH_REGISTER_LISTENER(MyTestPrinter)
 
 int main(int argc, char *argv[]) {
 
