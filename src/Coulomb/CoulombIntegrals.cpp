e4e91dba6684944482e8c04c649225e83b960110
andoniskoufris <skoufrisa@gmail.com>
2026-05-01 13:15:14 +1000

    Add frequency-dependent Breit less two-body matrix elements and direct potential term

--- src/Coulomb/CoulombIntegrals.cpp
+++ src/Coulomb/CoulombIntegrals.cpp
@@ -2,6 +2,8 @@
 #include "Angular/include.hpp"
 #include "Maths/Grid.hpp"
 #include "Maths/NumCalc_quadIntegrate.hpp"
+#include "Maths/SphericalBessel.hpp"
+#include "Physics/PhysConst_constants.hpp"
 #include "Wavefunction/DiracSpinor.hpp"
 #include "qip/Maths.hpp"
 #include "qip/Vector.hpp"
@@ -176,6 +178,251 @@ static inline void yk_ijk_gen_impl(const int l, const Function &ff,
 }
 
 //------------------------------------------------------------------------------
+
+// Used for frequency-dependent Breit
+template <int k, typename Function>
+static inline void
+yk_ijk_gen_impl_freq(const int l, const Function &ff, const Grid &gr,
+                     std::vector<double> &v0, std::vector<double> &vi,
+                     const std::size_t maxi, const double w) {
+  const auto du = gr.du();
+  const auto num_points = gr.num_points();
+  v0.resize(num_points); // for safety
+  vi.resize(num_points); // for safety
+  const auto irmax = (maxi == 0 || maxi > num_points) ? num_points : maxi;
+
+  // Quadrature integration weights:
+  const auto weights = [=](std::size_t i) {
+    if (i < NumCalc::Nquad)
+      return NumCalc::dq_inv * NumCalc::cq[i];
+    if (i < num_points - NumCalc::Nquad)
+      return 1.0;
+    return NumCalc::dq_inv * NumCalc::cq[num_points - i - 1];
+  };
+
+  const auto &r = gr.r();
+
+  auto jkwr_func = [](const int &l, const double &w, const double &r) {
+    return SphericalBessel::exactGSL_JL_alt(l, w * r);
+  };
+  auto ykwr_func = [](const int &l, const double &w, const double &r) {
+    return SphericalBessel::exactGSL_YL_alt(l, w * r);
+  };
+
+  // fills vector with spherical Bessel functions of first kind j_k(\alpha*\omega*r)
+  //const auto jkwr = SphericalBessel::fillBesselVec_kr_alt(l, w, r);
+
+  // fills vector with spherical Bessel functions of second kind y_k(\alpha*\omega*r)
+  //const auto ykwr = SphericalBessel::fillSecondBesselVec_kr_alt(l, w, r);
+
+  // values to keep running track of numerical integrals
+  double Ax = 0.0;
+  double Bx = 0.0;
+
+  // performs numerical integral from r'=0 to r'=r using single for loop trick
+  v0[0] = 0.0;
+  for (std::size_t i = 1; i < irmax; ++i) {
+    Ax = Ax + jkwr_func(k, w, r[i - 1]) * ff(i - 1) * weights(i - 1) *
+                gr.drdu(i - 1);
+    v0[i] = -w * (2 * k + 1.0) * ykwr_func(k, w, r[i]) * Ax * du;
+  }
+  for (std::size_t i = irmax; i < num_points; i++) {
+    v0[i] = 0.0;
+  }
+
+  // nb bmax may be num_points
+  const auto bmax = irmax;
+  for (std::size_t i = 0; i <= bmax; i++) {
+    vi[i] = 0.0;
+  }
+  const auto rbmax =
+    bmax == num_points ? r.back() + gr.drdu().back() * du : r[bmax];
+
+  Bx = Bx + ykwr_func(k, w, r[bmax - 1]) * ff(bmax - 1) * gr.drdu(bmax - 1);
+
+  vi[bmax - 1] = -w * (2.0 * k + 1.0) * jkwr_func(k, w, r[bmax - 1]) * Bx * du;
+
+  // loop for the integral that goes from r'=r up to r<infinity
+  // in this loop I have to shift the index up by 1 so that i goes down to i>=1 rather than i>=0, since it freaks out when I do the latter, for some reason
+  for (auto i = bmax - 1; i >= 1; i--) {
+    Bx = Bx + ykwr_func(k, w, r[i - 1]) * ff(i - 1) * weights(i - 1) *
+                gr.drdu(i - 1);
+    vi[i - 1] = -w * (2 * k + 1.0) * jkwr_func(k, w, r[i - 1]) * Bx * du;
+  }
+}
+
+//---------------------------------------------------------------------------------
+
+// function for calculating the Breit frequency-dependent screening factor vkabcd(w) - stores first term v1 and second term v2 separately
+template <int k>
+static inline void
+vkabcd_freqw(const int l, const std::vector<double> &Pkbd,
+             const std::vector<double> &Qkbd, const Grid &gr,
+             std::vector<double> &v1, std::vector<double> &v2,
+             std::vector<double> &v3, std::vector<double> &v4,
+             const std::size_t maxi, const double w) {
+
+  bool cut_off = w <= PhysConst::alpha;
+  // bool cut_off = true;
+  // bool cut_off = false;
+
+  const auto du = gr.du();
+  const auto num_points = gr.num_points();
+  v1.resize(num_points); // for safety
+  v2.resize(num_points); // for safety
+  v3.resize(num_points); // for safety
+  v4.resize(num_points); // for safety
+  const auto irmax = (maxi == 0 || maxi > num_points) ? num_points : maxi;
+
+  // faster method to calculate r^k
+  const auto powk = [l]() {
+    if constexpr (k < 0) {
+      return [l](double x) { return std::pow(x, l); };
+    } else {
+      (void)l; // l not used
+      return qip::pow<k, double>;
+    }
+  }();
+
+  // Quadrature integration weights:
+  const auto weights = [=](std::size_t i) {
+    if (i < NumCalc::Nquad)
+      return NumCalc::dq_inv * NumCalc::cq[i];
+    if (i < num_points - NumCalc::Nquad)
+      return 1.0;
+    return NumCalc::dq_inv * NumCalc::cq[num_points - i - 1];
+  };
+
+  const auto &r = gr.r();
+
+  auto jkwr = [](const int &l, const double &w, const double &r) {
+    // return SphericalBessel::exactGSL_JL_alt(l, w * r);
+    return SphericalBessel::exactGSL_JL_alt(l, w * r);
+  };
+  auto ykwr = [](const int &l, const double &w, const double &r) {
+    // return SphericalBessel::exactGSL_YL(l, w * r);
+    return SphericalBessel::exactGSL_YL_alt(l, w * r);
+  };
+
+  // values to keep running track of numerical integrals
+  double A1 = 0.0;
+  double A2 = 0.0;
+  double C1 = 0.0;
+  double A3 = 0.0;
+
+  const double wsd2 = w * w / 2.0;
+  const double wcubed = w * w * w;
+  const double odw2 = 1.0 / (w * w);
+
+  // performs numerical integrals for v1 and v2
+  v1[0] = 0.0;
+  v2[0] = 0.0;
+  for (std::size_t i = 1; i < irmax; ++i) {
+
+    double ratio = r[i - 1] / r[i];
+
+    //! This term numerically unstable for small omega
+    if (cut_off) {
+      A1 = (A1 + Pkbd[i - 1] * weights(i - 1) * gr.drduor(i - 1)) * powk(ratio);
+      A2 = (A2 + Pkbd[i - 1] * weights(i - 1) * gr.drduor(i - 1)) *
+           powk(ratio) * ratio * ratio;
+      C1 = (C1 + Pkbd[i - 1] * weights(i - 1) * r[i - 1] * gr.drdu(i - 1)) *
+           powk(ratio);
+      v1[i] = (A1 - A2 + wsd2 * C1) * du;
+    } else {
+      // first term in first part of v
+      A1 = A1 + jkwr(k - 1, w, r[i - 1]) * Pkbd[i - 1] * weights(i - 1) *
+                  gr.drdu(i - 1);
+
+      // second term in first part of v
+      A2 = (A2 + Pkbd[i - 1] * weights(i - 1) * gr.drduor(i - 1) * 1.0 /
+                   (r[i - 1] * r[i - 1])) *
+           powk(ratio) * ratio * ratio;
+
+      v1[i] = wcubed * ykwr(k + 1, w, r[i]) * A1 + (2.0 * k + 1.0) * A2;
+      v1[i] = -2.0 * odw2 * v1[i] * du;
+    }
+
+    // integral for second term in v
+    A3 = A3 + jkwr(k + 1, w, r[i - 1]) * Qkbd[i - 1] * weights(i - 1) *
+                gr.drdu(i - 1);
+    v2[i] = -2.0 * w * ykwr(k - 1, w, r[i]) * A3 * du;
+  }
+
+  for (std::size_t i = irmax; i < num_points; i++) {
+    v1[i] = 0.0;
+    v2[i] = 0.0;
+  }
+
+  double B1 = 0.0;
+  double B2 = 0.0;
+  double D1 = 0.0;
+  double B3 = 0.0;
+
+  // nb bmax may be num_points
+  const auto bmax = irmax;
+  for (std::size_t i = 0; i <= bmax; i++) {
+    v3[i] = 0.0;
+    v4[i] = 0.0;
+  }
+  const auto rbmax =
+    bmax == num_points ? r.back() + gr.drdu().back() * du : r[bmax];
+
+  // calculating screening functions at end point
+  if (cut_off) {
+    // B1 = Qkbd[bmax - 1] * weights(bmax - 1) * gr.drduor(bmax - 1);
+    // B2 = Qkbd[bmax - 1] * weights(bmax - 1) * gr.drduor(bmax - 1);
+    B1 = Qkbd[bmax - 1];
+    B2 = Qkbd[bmax - 1];
+    D1 = Qkbd[bmax - 1] * weights(bmax - 1) * r[bmax - 1] * gr.drdu(bmax - 1);
+    v3[bmax - 1] = (B1 - B2 + wsd2 * D1) * du;
+  } else {
+    B1 = B1 + ykwr(k + 1, w, r[bmax - 1]) * Qkbd[bmax - 1] * gr.drdu(bmax - 1);
+    B2 = B2 + (pow(r[bmax - 1], k - 1) / pow(r[bmax - 1], k + 2)) *
+                Qkbd[bmax - 1] * gr.drdu(bmax - 1);
+    v3[bmax - 1] = -2.0 * w * jkwr(k - 1, w, r[bmax - 1]) * B1 * du -
+                   2.0 * ((2.0 * k + 1.0) / (w * w)) * B2 * du;
+  }
+
+  B3 = B3 + ykwr(k - 1, w, r[bmax - 1]) * Pkbd[bmax - 1] * gr.drdu(bmax - 1);
+
+  v4[bmax - 1] = -2.0 * w * jkwr(k + 1, w, r[bmax - 1]) * B3 * du;
+
+  for (auto i = bmax - 1; i >= 1; i--) {
+
+    double ratio = r[i - 1] / r[i];
+
+    if (cut_off) {
+      B1 = B1 * powk(ratio) * (1.0 / ratio) +
+           Qkbd[i - 1] * weights(i - 1) * gr.drduor(i - 1);
+      B2 = B2 * powk(ratio) * ratio +
+           Qkbd[i - 1] * weights(i - 1) * gr.drduor(i - 1);
+      D1 = D1 * powk(ratio) * ratio +
+           Qkbd[i - 1] * weights(i - 1) * r[i - 1] * gr.drdu(i - 1);
+      v3[i - 1] = (B1 - B2 + wsd2 * D1) * du;
+    } else {
+      // j_{k-1}(wr1)y_{k+1}(wr2) term in integral that is integrated from r1 <= r2 <=infty
+      B1 = B1 + ykwr(k + 1, w, r[i - 1]) * Qkbd[i - 1] * weights(i - 1) *
+                  gr.drdu(i - 1);
+
+      // r2^{k-1}/r1^{k+2} term in integral that is integrated from r1 <= r2 <=infty
+      B2 = powk(ratio) * (1.0 / ratio) * B2 + Qkbd[i - 1] * weights(i - 1) *
+                                                gr.drduor(i - 1) /
+                                                (r[i - 1] * r[i - 1]);
+
+      v3[i - 1] = wcubed * jkwr(k - 1, w, r[i - 1]) * B1 + (2.0 * k + 1.0) * B2;
+
+      v3[i - 1] = -2.0 * odw2 * v3[i - 1] * du;
+    }
+
+    // j_{k+1}(wr1)y_{k-1}(wr2) term in integral that is integrated from r1 <= r2 <=infty
+    B3 = B3 + ykwr(k - 1, w, r[i - 1]) * Pkbd[i - 1] * weights(i - 1) *
+                gr.drdu(i - 1);
+    v4[i - 1] = -2.0 * w * jkwr(k + 1, w, r[i - 1]) * B3 * du;
+  }
+}
+
+//------------------------------------------------------------------------------
 std::vector<double> yk_ab(const int k, const DiracSpinor &Fa,
                           const DiracSpinor &Fb, const std::size_t maxi) {
   std::vector<double> ykab; //
@@ -291,6 +538,113 @@ void gk_ab(const int k, const DiracSpinor &Fa, const DiracSpinor &Fb,
     yk_ijk_gen_impl<8>(k, fgfg, Fa.grid(), g0, ginf, maxi);
   else
     yk_ijk_gen_impl<-1>(k, fgfg, Fa.grid(), g0, ginf, maxi);
+}
+
+//--------------------------------------------------------------------
+
+// frequency-dependent Breit integrals which i call g^k_{ab}(r,w) and h^k_{ab}(r,w) for X_{ij} and Y_{ij}, respectively
+// also need to define v^{k,1}_{ab} and v^k{k,2}_{ab} for the two respective terms in v^k_{ab} which will be multiplying different parts of the
+void gk_ab_freqw(const int k, const DiracSpinor &Fa, const DiracSpinor &Fb,
+                 std::vector<double> &g0, std::vector<double> &ginf,
+                 const std::size_t maxi, const double w) {
+
+  auto fgfg = [&](std::size_t i) {
+    return (Fa.f(i) * Fb.g(i) + Fa.g(i) * Fb.f(i));
+  };
+
+  if (k == 0)
+    yk_ijk_gen_impl_freq<0>(k, fgfg, Fa.grid(), g0, ginf, maxi, w);
+  else if (k == 1)
+    yk_ijk_gen_impl_freq<1>(k, fgfg, Fa.grid(), g0, ginf, maxi, w);
+  else if (k == 2)
+    yk_ijk_gen_impl_freq<2>(k, fgfg, Fa.grid(), g0, ginf, maxi, w);
+  else if (k == 3)
+    yk_ijk_gen_impl_freq<3>(k, fgfg, Fa.grid(), g0, ginf, maxi, w);
+  else if (k == 4)
+    yk_ijk_gen_impl_freq<4>(k, fgfg, Fa.grid(), g0, ginf, maxi, w);
+  else if (k == 5)
+    yk_ijk_gen_impl_freq<5>(k, fgfg, Fa.grid(), g0, ginf, maxi, w);
+  else if (k == 6)
+    yk_ijk_gen_impl_freq<6>(k, fgfg, Fa.grid(), g0, ginf, maxi, w);
+  else if (k == 7)
+    yk_ijk_gen_impl_freq<7>(k, fgfg, Fa.grid(), g0, ginf, maxi, w);
+  else if (k == 8)
+    yk_ijk_gen_impl_freq<8>(k, fgfg, Fa.grid(), g0, ginf, maxi, w);
+  else
+    yk_ijk_gen_impl_freq<-1>(k, fgfg, Fa.grid(), g0, ginf, maxi, w);
+}
+
+void hk_ab_freqw(const int k, const DiracSpinor &Fa, const DiracSpinor &Fb,
+                 std::vector<double> &b0, std::vector<double> &binf,
+                 std::size_t maxi, const double w) {
+
+  auto fgfg = [&](std::size_t i) {
+    return (Fa.f(i) * Fb.g(i) - Fa.g(i) * Fb.f(i));
+  };
+
+  if (k == 0)
+    yk_ijk_gen_impl_freq<0>(k, fgfg, Fa.grid(), b0, binf, maxi, w);
+  else if (k == 1)
+    yk_ijk_gen_impl_freq<1>(k, fgfg, Fa.grid(), b0, binf, maxi, w);
+  else if (k == 2)
+    yk_ijk_gen_impl_freq<2>(k, fgfg, Fa.grid(), b0, binf, maxi, w);
+  else if (k == 3)
+    yk_ijk_gen_impl_freq<3>(k, fgfg, Fa.grid(), b0, binf, maxi, w);
+  else if (k == 4)
+    yk_ijk_gen_impl_freq<4>(k, fgfg, Fa.grid(), b0, binf, maxi, w);
+  else if (k == 5)
+    yk_ijk_gen_impl_freq<5>(k, fgfg, Fa.grid(), b0, binf, maxi, w);
+  else if (k == 6)
+    yk_ijk_gen_impl_freq<6>(k, fgfg, Fa.grid(), b0, binf, maxi, w);
+  else if (k == 7)
+    yk_ijk_gen_impl_freq<7>(k, fgfg, Fa.grid(), b0, binf, maxi, w);
+  else if (k == 8)
+    yk_ijk_gen_impl_freq<8>(k, fgfg, Fa.grid(), b0, binf, maxi, w);
+  else
+    yk_ijk_gen_impl_freq<-1>(k, fgfg, Fa.grid(), b0, binf, maxi, w);
+}
+
+void vk_ab_freqw(const int k, const DiracSpinor &Fi, const DiracSpinor &Fj,
+                 const Grid &gr, std::vector<double> &v1,
+                 std::vector<double> &v2, std::vector<double> &v3,
+                 std::vector<double> &v4, std::size_t maxi, const double w) {
+
+  auto Xij = [&](std::size_t i) {
+    return (Fi.f(i) * Fj.g(i) + Fi.g(i) * Fj.f(i));
+  };
+
+  auto Yij = [&](std::size_t i) {
+    return (Fi.f(i) * Fj.g(i) - Fi.g(i) * Fj.f(i));
+  };
+
+  std::vector<double> Pkbd(gr.size(), 0.0);
+  std::vector<double> Qkbd(gr.size(), 0.0);
+
+  for (int i = 0; i < gr.size(); i++) {
+    Pkbd[i] = ((Fi.kappa() - Fj.kappa()) / k) * Xij(i) - Yij(i);
+    Qkbd[i] = ((Fi.kappa() - Fj.kappa()) / (k + 1.0)) * Xij(i) + Yij(i);
+  }
+
+  if (k == 0)
+    vkabcd_freqw<0>(k, Pkbd, Qkbd, gr, v1, v2, v3, v4, maxi, w);
+  else if (k == 1)
+    vkabcd_freqw<1>(k, Pkbd, Qkbd, gr, v1, v2, v3, v4, maxi, w);
+  else if (k == 2)
+    vkabcd_freqw<2>(k, Pkbd, Qkbd, gr, v1, v2, v3, v4, maxi, w);
+  else if (k == 3)
+    vkabcd_freqw<3>(k, Pkbd, Qkbd, gr, v1, v2, v3, v4, maxi, w);
+  else if (k == 4)
+    vkabcd_freqw<4>(k, Pkbd, Qkbd, gr, v1, v2, v3, v4, maxi, w);
+  else if (k == 5)
+    vkabcd_freqw<5>(k, Pkbd, Qkbd, gr, v1, v2, v3, v4, maxi, w);
+  else if (k == 6)
+    vkabcd_freqw<6>(k, Pkbd, Qkbd, gr, v1, v2, v3, v4, maxi, w);
+  else if (k == 7)
+    vkabcd_freqw<7>(k, Pkbd, Qkbd, gr, v1, v2, v3, v4, maxi, w);
+  else if (k == 8)
+    vkabcd_freqw<8>(k, Pkbd, Qkbd, gr, v1, v2, v3, v4, maxi, w);
+  else
+    vkabcd_freqw<-1>(k, Pkbd, Qkbd, gr, v1, v2, v3, v4, maxi, w);
 }
 
 //==============================================================================
