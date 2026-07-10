319984ab606931e808c08e4996f87e929eac12c5
JackBunya <uqjeasto@uq.edu.au>
2026-07-10 12:26:36 +1000

    fixed structure radiation

--- src/MBPT/StructureRad.cpp
+++ src/MBPT/StructureRad.cpp
@@ -76,6 +76,9 @@ StructureRad::StructureRad(const std::vector<DiracSpinor> &basis,
     if (verbose)
       std::cout << "\nUsing Yk Coulomb table for Struc. Rad.\n";
     mY.calculate(mBasis);
+    mQ->fill(mBasis, mY, k_cut, verbose);
+    // mQ->fill_if(mBasis, mY, select_Q_SR, k_cut, verbose);
+    mQ->write(Qk_fname, verbose);
   }
 }
 
